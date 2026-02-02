99fcc48e1d7857108f5dfbbc01038a4026e250f9
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-02 16:07:07 -0300

    feat: add Currency enum with support for USD, BRL, and EUR

---
+++ src/Snebur.Core/Enums/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Snebur.Core.Enums;
+
+public enum Currency
+{
+    [UndefinedValue]
+    Unknown = 0,
+
+    [Description("US Dollar (USD)")]
+    USD = 840,
+
+    [Description("Brazilian Real (BRL)")]
+    BRL = 986,
+
+    [Description("Euro (EUR)")]
+    EUR = 978
+}
