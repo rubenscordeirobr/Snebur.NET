5eb7fb6cbefe28d983e19aa5378a783bc8ded674
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-03 09:03:00 -0300

    feat: add CaseType enum to represent various naming conventions

---
+++ src/Snebur.Core/Enums/CaseType.cs
@@ -0,0 +1,18 @@
+﻿namespace Snebur.Core.Enums;
+
+public enum CaseType
+{
+    Unknown,
+    LowerCase,
+    PascalCase,
+    CamelCase,
+    SnakeCase,
+    KebabCase,
+    ScreamingSnakeCase,
+    ScreamingKebabCase,
+    TitleSnakeCase,
+    TitleKebabCase,
+    CamelSnakeCase,
+    CamelKebabCase,
+    UpperCase,
+}
