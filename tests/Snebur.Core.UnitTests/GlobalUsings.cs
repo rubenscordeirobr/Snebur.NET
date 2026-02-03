8ef446b8b5937e6f21a0109d19823c46b53f7bbf
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-03 07:15:14 -0300

    feat(tests): add unit test project for Snebur.Core

---
+++ tests/Snebur.Core.UnitTests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Snebur.Core.Enums;
+global using Snebur.Core.Utils;
+global using FluentAssertions;
+global using Snebur.Core.Extensions;
+global using Snebur.Core.Infos;
+global using Xunit;
