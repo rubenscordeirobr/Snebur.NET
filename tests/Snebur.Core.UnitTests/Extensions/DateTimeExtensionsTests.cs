d29fb24b341c8067bfec26fea62c9cabf84b7a01
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-11 18:33:19 -0300

    feat: add DateTimeExtensions with IsExpired and IsCloseTo methods

---
+++ tests/Snebur.Core.UnitTests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Snebur.Core.UnitTests.Extensions;
+
+public class DateTimeExtensionsTests
+{
+    [Theory]
+    [InlineData(-1, true)]
+    [InlineData(0, true)]
+    [InlineData(1, false)]
+    public void IsExpired_ShouldReturnExpectedResult(int secondsToAdd, bool expectedResult)
+    {
+        // Arrange
+        var dateTime = DateTime.UtcNow.AddSeconds(secondsToAdd);
+        var expirationTime = TimeSpan.FromSeconds(0);
+
+        // Act
+        var result = dateTime.IsExpired(expirationTime);
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+}
