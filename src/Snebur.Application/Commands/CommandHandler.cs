3d371171dd742705ac9a92bc8da3ca34512ade1a
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-12 19:15:10 -0300

    feat(application): add base CommandHandler implementation

---
+++ src/Snebur.Application/Commands/CommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Snebur.Application.Abstractions.Handlers;
+
+namespace Snebur.Application.Commands;
+
+public abstract class CommandHandler<TRequest, TResponse>
+    : ICommandHandler<TRequest, TResponse>
+    where TRequest : ICommandRequest<TResponse>
+    where TResponse : IResponse
+{
+    public Task<Result<TResponse>> RunAsync(
+        TRequest command,
+        CancellationToken cancellationToken = default)
+    {
+        return HandleAsync(command, cancellationToken);
+    }
+
+    protected abstract Task<Result<TResponse>> HandleAsync(
+        TRequest command,
+        CancellationToken cancellationToken);
+}
