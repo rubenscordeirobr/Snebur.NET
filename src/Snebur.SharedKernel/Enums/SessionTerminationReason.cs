1ff56af6a5ec77a0dd4b9bb490b06c7523257cc7
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-12 18:34:29 -0300

    feat: add enums for session termination and user notification types

---
+++ src/Snebur.SharedKernel/Enums/SessionTerminationReason.cs
@@ -0,0 +1,13 @@
+﻿namespace Snebur.SharedKernel.Enums;
+
+public enum SessionTerminationReason
+{
+    [UndefinedValue]
+    Unknown = 0,
+    IpAddressChanged,
+    UserAgentChanged,
+    PasswordChanged,
+    SessionExpired,
+    DomainEventError,
+    UserLogout,
+}
---
+++ src/Snebur.SharedKernel/Enums/UserNotificationType.cs
@@ -0,0 +1,10 @@
+﻿namespace Snebur.SharedKernel.Enums;
+
+public enum UserNotificationType
+{
+    [UndefinedValue]
+    Undefined = 0,
+    NewUser = 1,
+    PasswordReset,
+    AutehnticationFailed
+}
