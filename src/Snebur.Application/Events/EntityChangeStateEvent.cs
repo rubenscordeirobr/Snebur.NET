7d2547a4c87c758ae3a4697229a04ba064ab13aa
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-13 09:26:24 -0300

    feat(events): add initial domain event models and handlers

---
+++ src/Snebur.Application/Events/EntityChangeStateEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Snebur.Application.Events;
+
+public abstract record EntityChangeStateEvent<TEntity>: IEntityStateChangedEvent<TEntity>
+    where TEntity : EntityBase
+{
+    public TEntity Entity { get; }
+    public abstract EntityChangeState State { get; }
+    
+    protected EntityChangeStateEvent(TEntity entity)
+    {
+        Entity = entity;
+    }
+}
---
+++ src/Snebur.Application/Events/ExecutedDomainEventResult.cs
@@ -0,0 +1,14 @@
+﻿using Snebur.Application.Abstractions.Handlers;
+
+namespace Snebur.Application.Events;
+
+public sealed record ExecutedDomainEventResult(
+    IDomainEvent DomainEvent,
+    Type HandlerType,
+    Type? ImplementationHandlerType,
+    IApplicationHandler? Handler,
+    Exception? Exception)
+{
+    public bool IsSuccess 
+        => Exception is null;
+}
