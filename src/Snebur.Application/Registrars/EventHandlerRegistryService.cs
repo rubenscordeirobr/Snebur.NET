bf9edee784fcd11cff4a844febe123100c16641c
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-14 10:47:28 -0300

    feat: add EventHandlerRegistryService for managing event handler mappings

---
+++ src/Snebur.Application/Registrars/EventHandlerRegistryService.cs
@@ -0,0 +1,33 @@
+﻿using Snebur.Application.Abstractions.Registrars;
+
+namespace Snebur.Application.Registrars;
+
+internal class EventHandlerRegistryService : IEventHandlerRegistryService
+{
+    private readonly HandlerMappings _domainEventHandlerMappings = new(HandlerKind.DomainEventHandler);
+    private readonly HandlerMappings _domainEventPreProcessorHandlerMappings = new(HandlerKind.PreProcessorHandler);
+
+    public void MapperDomainEventHandler(
+        Type domainEventType,
+        Type handlerType)
+    {
+        _domainEventHandlerMappings.MapperEventHandler(domainEventType, handlerType);
+    }
+
+    public void MapperDomainEventPreProcessorHandler(
+        Type domainEventType,
+        Type handlerType)
+    {
+        _domainEventPreProcessorHandlerMappings.MapperEventHandler(domainEventType, handlerType);
+    }
+
+    public IReadOnlyList<Type> GetDomainEventHandlers(Type eventType)
+    {
+        return _domainEventHandlerMappings.GetHandlerTypes(eventType);
+    }
+
+    public IReadOnlyList<Type> GetDomainEventPreProcessorHandlers(Type eventType)
+    {
+        return _domainEventPreProcessorHandlerMappings.GetHandlerTypes(eventType);
+    }
+}
