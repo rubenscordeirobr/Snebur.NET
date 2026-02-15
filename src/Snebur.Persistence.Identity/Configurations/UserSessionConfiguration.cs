dacb629fc15a27e3d591d2864dc6999e1ba30bc6
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 16:28:28 -0300

    feat(identity): add UserSessionConfiguration for entity configuration

---
+++ src/Snebur.Persistence.Identity/Configurations/UserSessionConfiguration.cs
@@ -0,0 +1,40 @@
+﻿namespace Snebur.Persistence.Identity.Configurations;
+
+public class UserSessionConfiguration : IEntityTypeConfiguration<UserSession>
+{
+    public void Configure(EntityTypeBuilder<UserSession> builder)
+    {
+        Guard.NotNull(builder);
+          
+        builder.Property(x => x.ApplicationName)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.NameMaxLength);
+
+        builder.Property(x => x.IpAddress)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.IpAddressMaxLength);
+         
+        builder.Property(x => x.UserAgent)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.UserAgentMaxLength);
+
+        builder.Property(x => x.StartedAt)
+            .HasDefaultValueSql("now()")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.LastActivity)
+            .HasDefaultValueSql("now()")
+            .ValueGeneratedOnAdd();
+
+        builder.HasOne(x => x.User)
+           .WithMany(x => x.Sessions)
+           .HasForeignKey(x => x.User_Id)
+           .IsRequired(false);
+
+        builder.HasOne(x => x.Tenant)
+           .WithMany(x => x.Sessions)
+           .HasForeignKey(x => x.Tenant_Id)
+           .IsRequired(false);
+         
+    }
+}
