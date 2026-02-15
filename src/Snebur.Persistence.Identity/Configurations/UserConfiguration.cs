5b23b8ce80838f2ff3e6c7323efa5675be9c61c2
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 16:27:39 -0300

    feat(identity): add UserConfiguration for entity type configuration

---
+++ src/Snebur.Persistence.Identity/Configurations/UserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Snebur.Persistence.Identity.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        Guard.NotNull(builder);
+
+        builder.Property(x => x.Role)
+          .IsRequired();
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.NameMaxLength);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.EmailMaxLength);
+    }
+}
