bfea957d836fed8d4d408a249639078c0d1ce0f5
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 16:29:14 -0300

    feat(identity): add UserSessionRepository implementation

---
+++ src/Snebur.Persistence.Identity/Repositories/UserSessionRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace Snebur.Persistence.Identity.Repositories;
+
+internal class UserSessionRepository : RepositoryBase<UserSession>, IUserSessionRepository
+{
+    public UserSessionRepository(
+        IdentityDbContext dbContext,
+        IHttpContextSessionAccessor userSessionAccessor,
+        TrackingOption trackingOption = TrackingOption.NoTracking)
+        : base(dbContext, userSessionAccessor, trackingOption)
+    {
+    }
+
+    
+    public Task<UserSession?> GetByIdWithUserAsync(
+        Guid session_Id,
+        CancellationToken cancellationToken = default)
+    {
+        return GetByIdAsync(session_Id,
+                cancellationToken,
+                userSession => userSession.User);
+    }
+}
