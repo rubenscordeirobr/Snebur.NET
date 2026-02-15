cde89b8a70af08467cfdde5258e4f7afc5f9d534
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:05:12 -0300

    feat: add default model builder configuration for entity setup

---
+++ src/Snebur.Persistence.Common/Configurations/ModelBuilderConfiguration.cs
@@ -0,0 +1,54 @@
+﻿namespace Snebur.Persistence.Common.Configurations;
+
+public static partial class ModelBuilderConfiguration
+{
+    public static ModelBuilder ConfigureModelDefaultConfiguration<TContext>(
+        this ModelBuilder modelBuilder,
+        bool isInMemory)
+        where TContext : DbContext
+    {
+        Guard.NotNull(modelBuilder);
+
+        modelBuilder.HasPostgresExtension("uuid-ossp");
+
+        modelBuilder.HasCollation("case_accent_insensitive",
+            locale: "und-u-ks-level1",
+            provider: "icu",
+            deterministic: false);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TContext).Assembly)
+            .UseCollation("case_accent_insensitive");
+
+        modelBuilder.UseSnakeCaseNamingConvention();
+
+        var entitiesType = modelBuilder.Model.GetEntityTypes();
+
+        foreach (var mutableEntityType in entitiesType)
+        {
+            var entityType = mutableEntityType.ClrType;
+            if (!entityType.IsSubclassOf<EntityBase>())
+            {
+                continue;
+            }
+
+            var entityBuilder = modelBuilder.Entity(entityType);
+            if (entityBuilder == null)
+            {
+                throw new EntityBuilderNotFoundException($"Entity builder not found for {mutableEntityType.ClrType.Name}");
+            }
+
+            entityBuilder.ConfigureEntityDefaultSettings(isInMemory);
+
+            mutableEntityType
+                .ConfigureValueObjects(entityBuilder, isInMemory)
+                .ConfigureProperties();
+
+            mutableEntityType.Validate<TContext>();
+        }
+
+        // Reapply naming convention to ensure consistency after potential index or configuration changes
+        modelBuilder.UseSnakeCaseNamingConvention();
+
+        return modelBuilder;
+    }
+}
