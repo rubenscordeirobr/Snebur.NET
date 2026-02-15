25928eb2fc310593c95855e2dc0ed8f0c08ef3e1
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:03:17 -0300

    feat: add service configuration for application and shared use cases

---
+++ src/Snebur.Application/ApplicationServiceConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Snebur.Application.Registrars;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Snebur.Application;
+
+public static class ApplicationServiceConfiguration
+{
+    public static IServiceCollection AddApplicationServices(
+        this IServiceCollection services)
+    {
+  
+        services.AddApplicationHandlersFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationHandlersFromAssembly(
+       this IServiceCollection services,
+       Assembly assembly)
+    {
+        Guard.NotNull(assembly);
+
+        var serviceRegistrar = new ApplicationHandlerRegistrar(services);
+        serviceRegistrar.RegisterFromAssembly(assembly);
+        return services;
+    }
+
+    internal static IServiceCollection AddApplicationHandlersFromTypes(
+       this IServiceCollection services,
+       Type[] handlerTypes)
+    {
+        var serviceRegistrar = new ApplicationHandlerRegistrar(services);
+        serviceRegistrar.RegisterHandlerFromTypes(handlerTypes);
+        return services;
+    }
+}
---
+++ src/Snebur.UseCases.Shared/SharedUseCasesServiceConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Snebur.UseCases.Common.Registrars;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Snebur.UseCases;
+
+public static class SharedUseCasesServiceConfiguration
+{
+    public static IServiceCollection AddUserCasesSharedServices(
+        this IServiceCollection services )
+    {
+
+        ValidatorOptions.Global.DefaultClassLevelCascadeMode = CascadeMode.Stop;
+        ValidatorOptions.Global.DefaultRuleLevelCascadeMode = CascadeMode.Stop;
+         
+        services.AddCommandValidationServicesFromAssembly(Assembly.GetExecutingAssembly());
+        return services;
+    }
+
+    public static IServiceCollection AddCommandValidationServicesFromAssembly(
+       this IServiceCollection services,
+       Assembly assembly)
+    {
+        Guard.NotNull(assembly);
+
+        var validationRegistrar = new CommandValidationRegistrar(services);
+        validationRegistrar.RegisterFromAssembly(assembly);
+        return services;
+    }
+
+    internal static IServiceCollection AddCommandValidationServicesFromTypes(
+        this IServiceCollection services,
+        Type[] types)
+    {
+        var validationRegistrar = new CommandValidationRegistrar(services);
+        validationRegistrar.RegisterFromTypes(types);
+        return services;
+    }
+}
