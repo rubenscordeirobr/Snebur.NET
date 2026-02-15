97d2f408a87fbeb3cec3d1ab8d36b93fb251406b
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 17:59:24 -0300

    feat: add IdentityDbContext with seed capabilities

---
+++ src/Snebur.Persistence.Identity/IdentityDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Snebur.Application.Exceptions;
+using Snebur.Persistence.Common.Configurations;
+
+namespace Snebur.Persistence.Identity;
+
+internal class IdentityDbContext : DbContext, IDbSeedAsync
+{
+    internal DbSet<User> Users { get; set; }
+    internal DbSet<UserSession> Sessions { get; set; }
+    internal DbSet<Tenant> Tenants { get; set; }
+    internal DbSet<TenantAddress> Addresses { get; set; }
+
+    public IdentityDbContext(
+        DbContextOptions<IdentityDbContext> options ) 
+        : base(options)
+    {
+    }
+
+    public override int SaveChanges()
+    {
+        throw new SyncSaveChangesNotAllowedException("Use SaveChangesAsync instead");
+    }
+
+    public override async Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default)
+    {
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+  
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ConfigureModelDefaultConfiguration<IdentityDbContext>(isInMemory:false);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    #region IDbSeedAsync 
+
+    async Task<int> IDbSeedAsync.SeedSaveChangesAsync()
+    {
+        if (await this.Users.AnyAsync())
+        {
+            throw new InvalidOperationException("Database already seeded");
+        }
+        return await base.SaveChangesAsync();
+    } 
+    #endregion
+}
+
+internal interface IDbSeedAsync
+{
+    Task<int> SeedSaveChangesAsync();
+}
