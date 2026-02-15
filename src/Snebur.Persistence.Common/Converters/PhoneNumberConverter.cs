601725df08245b9ebd12cd5161d5752598ccb0c6
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:49:08 -0300

    feat: add PhoneNumberConverter for value conversion

---
+++ src/Snebur.Persistence.Common/Converters/PhoneNumberConverter.cs
@@ -0,0 +1,15 @@
+﻿using Snebur.SharedKernel.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Snebur.Persistence.Common.Converters;
+
+internal class PhoneNumberConverter : ValueConverter<PhoneNumber, string>
+{
+    private PhoneNumberConverter() : base(
+        phoneNumber => phoneNumber.FullNumber,
+        value => new PhoneNumber(value) )
+    {
+    }
+
+    internal static PhoneNumberConverter Instance { get; } = new();
+}
