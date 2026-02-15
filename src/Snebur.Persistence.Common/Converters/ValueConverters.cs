b8cb447019954dc1c096e2af7c2ee46cb314c667
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:49:23 -0300

    feat: add ValueConverters class with common value converters

---
+++ src/Snebur.Persistence.Common/Converters/ValueConverters.cs
@@ -0,0 +1,19 @@
+﻿namespace Snebur.Persistence.Common.Converters;
+
+internal static class ValueConverters
+{
+    internal static PhoneNumberConverter PhoneNumberConverter
+        => PhoneNumberConverter.Instance;
+
+    internal static FiscalCodeConverter FiscalCodeConverter
+        => FiscalCodeConverter.Instance;
+
+    internal static NullableUtcDateTimeConverter NullableUtcDateTimeConverter
+        => NullableUtcDateTimeConverter.Instance;
+
+    internal static UtcDateTimeConverter UtcDateTimeConverter 
+        => UtcDateTimeConverter.Instance;
+
+    internal static NullableGuidValueConverter EmptyNullableGuidConverter
+        => NullableGuidValueConverter.Instance;
+}
