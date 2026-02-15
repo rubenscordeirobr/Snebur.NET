944ad5f0f35f44f6fd6984b2243b037ec0183ed2
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:48:51 -0300

    feat: add UtcDateTimeConverter and NullableUtcDateTimeConverter classes

---
+++ src/Snebur.Persistence.Common/Converters/NullableUtcDateTimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Snebur.Persistence.Common.Converters;
+
+internal class NullableUtcDateTimeConverter : ValueConverter<DateTime?, DateTime?>
+{
+    private NullableUtcDateTimeConverter() : base(
+        v => ConverterUtcDateTime(v),
+        v => ConverterUtcDateTime(v))
+    {
+    }
+
+    private static DateTime? ConverterUtcDateTime(DateTime? dateTime)
+    {
+        if (dateTime == null || dateTime.Value == default )
+        {
+            return null;
+        }
+
+        return dateTime.Value.Kind == DateTimeKind.Utc
+            ? dateTime
+            : DateTime.SpecifyKind(dateTime.Value, DateTimeKind.Utc);
+    }
+
+    public static NullableUtcDateTimeConverter Instance { get; } = new NullableUtcDateTimeConverter();
+}
+
---
+++ src/Snebur.Persistence.Common/Converters/UtcDateTimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Snebur.Persistence.Common.Converters;
+
+internal class UtcDateTimeConverter : ValueConverter<DateTime, DateTime>
+{
+    private UtcDateTimeConverter() : base(
+        v => ConverterUtcDateTime(v),
+        v => ConverterUtcDateTime(v))
+    {
+    }
+     
+    private static DateTime ConverterUtcDateTime(DateTime dateTime)
+    {
+        if(dateTime == default)
+        {
+            return DateTime.SpecifyKind(default, DateTimeKind.Utc);
+        }
+
+        return dateTime.Kind == DateTimeKind.Utc 
+            ? dateTime 
+            : DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+    }
+
+    internal static UtcDateTimeConverter Instance { get; } = new UtcDateTimeConverter();
+}
+
