52d5069c18e427931d4279b804e35de5adc9e9e9
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:07:43 -0300

    feat: add extension to apply snake_case naming convention

---
+++ src/Snebur.Persistence.Common/Configurations/UseSnakeCaseNamingConvention.cs
@@ -0,0 +1,51 @@
+﻿using Snebur.Core.Utils;
+
+namespace Snebur.Persistence.Common.Configurations;
+
+internal static class UseSnakeCaseNamingConventionExtensions
+{
+    internal static void UseSnakeCaseNamingConvention(this ModelBuilder modelBuilder)
+    {
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            var tableName = CaseConventionUtils.ToSnakeCase(entity.GetTableName() ?? string.Empty);
+            entity.SetTableName(tableName);
+
+            foreach (var property in entity.GetProperties())
+            {
+                var columnName = property.GetColumnName();
+                if (!string.IsNullOrWhiteSpace(columnName))
+                {
+                    property.SetColumnName(CaseConventionUtils.ToSnakeCase(columnName));
+                }
+            }
+
+            foreach (var index in entity.GetIndexes())
+            {
+                var indexName = index.GetDatabaseName();
+                if (!string.IsNullOrWhiteSpace(indexName))
+                {
+                    index.SetDatabaseName(CaseConventionUtils.ToSnakeCase(indexName));
+                }
+            }
+
+            foreach (var key in entity.GetKeys())
+            {
+                var keyName = key.GetName();
+                if (!string.IsNullOrWhiteSpace(keyName))
+                {
+                    key.SetName(CaseConventionUtils.ToSnakeCase(keyName));
+                }
+            }
+
+            foreach (var foreignKey in entity.GetForeignKeys())
+            {
+                var foreignKeyName = foreignKey.GetConstraintName();
+                if (!string.IsNullOrWhiteSpace(foreignKeyName))
+                {
+                    foreignKey.SetConstraintName(CaseConventionUtils.ToSnakeCase(foreignKeyName));
+                }
+            }
+        }
+    }
+}
