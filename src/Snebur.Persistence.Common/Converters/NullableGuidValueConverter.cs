6ae7a57ae2d5493fb4420da4c261a527c2ea94a5
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:48:40 -0300

    feat: add NullableGuidValueConverter for Guid? value conversion

---
+++ src/Snebur.Persistence.Common/Converters/NullableGuidValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Snebur.Persistence.Common.Converters;
+
+public class NullableGuidValueConverter : ValueConverter<Guid?, Guid?>
+{
+    private NullableGuidValueConverter() : base(
+        guid => Convert(guid),
+        guid => Convert(guid))
+    {
+    }
+
+    internal static Guid? Convert(Guid? value)
+    {
+        return value == null || value.Value == Guid.Empty
+            ? null
+            : value;
+    }
+
+    public static NullableGuidValueConverter Instance { get; } = new();
+}
