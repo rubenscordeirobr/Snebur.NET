fd97414dea13b82b7a93f7878fef6882e06ddb6f
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:58:07 -0300

    feat: add NotEmptyGuidValidation to enforce non-empty Guid values

---
+++ src/Snebur.Persistence.Common/Validations/NotEmptyGuidValidation.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Snebur.Persistence.Common.Validations;
+
+public class NotEmptyGuidValidation : ValueConverter<Guid, Guid>
+{
+    public NotEmptyGuidValidation(PropertyInfo property) : base(
+        guid => Convert(guid, property),
+        guid => Convert(guid, property))
+    {
+    }
+
+    internal static Guid Convert(Guid value, PropertyInfo property)
+    {
+        if (value == Guid.Empty)
+        {
+            throw new ArgumentException($"Guid value {property.GetPropertyPath()} cannot be empty.");
+        }
+        return value;
+    }
+}
