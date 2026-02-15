57ee6fe1e644eae094da943045a599bed1619908
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:58:29 -0300

    feat: add EFExtensions with tracking and change detection methods

---
+++ src/Snebur.Persistence.Common/Extensions/EFExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Snebur.Persistence.Common.Enums;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Snebur.Persistence.Common.Extensions;
+
+public static class EFExtensions
+{
+    public static IQueryable<TEntity> ApplyTracking<TEntity>(
+         this IQueryable<TEntity> dbSet,
+         TrackingOption tracking)
+         where TEntity : EntityBase
+    {
+        return tracking == TrackingOption.Tracking
+            ? dbSet
+            : dbSet.AsNoTracking();
+    }
+
+    public static bool HasChanges(this EntityEntry entry)
+    {
+        Guard.NotNull(entry);
+
+        return entry.State switch
+        {
+            EntityState.Added => true,
+            EntityState.Modified => true,
+            EntityState.Deleted => true,
+            _ => false
+        };
+    }
+
+    public static bool IsInMemory(this DatabaseFacade database)
+          => database?.ProviderName == "Microsoft.EntityFrameworkCore.InMemory";
+
+}
+
