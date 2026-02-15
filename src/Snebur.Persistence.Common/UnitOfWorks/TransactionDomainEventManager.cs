c0e48685867980ff4e255eb3783d5bc057afec92
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-15 14:57:37 -0300

    feat: add TransactionDomainEventManager to manage domain event contexts

---
+++ src/Snebur.Persistence.Common/UnitOfWorks/TransactionDomainEventManager.cs
@@ -0,0 +1,24 @@
+﻿namespace Snebur.Persistence.Common.UnitOfWorks;
+
+internal class TransactionDomainEventManager(IUserSession userSession)
+{
+    internal readonly List<DomainEventContext> _domainEventContexts = new();
+
+    internal DomainEventContext GetDomainEventContext()
+        => CreateDomainEventContext();
+
+    internal void Add(DomainEventContext domainEventContext)
+    {
+        _domainEventContexts.Add(domainEventContext);
+    }
+
+    private DomainEventContext CreateDomainEventContext()
+    {
+        var events = _domainEventContexts
+            .SelectMany(domainEventContext => domainEventContext.Events)
+            .Distinct()
+            .ToList();
+
+        return new DomainEventContext(userSession, events);
+    }
+}
