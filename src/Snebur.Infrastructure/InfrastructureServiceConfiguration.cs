7d43f98e51177133ff28059d7a356db5179708bc
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-16 12:24:16 -0300

    feat: add service configuration for infrastructure and use cases modules

---
+++ src/Snebur.Infrastructure/InfrastructureServiceConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Snebur.Application.Abstractions.Security;
+using Snebur.Infrastructure.Cache;
+using Snebur.Infrastructure.Configurations;
+using Snebur.Infrastructure.Factories;
+using Snebur.Infrastructure.Services;
+using Snebur.SharedKernel.Abstractions;
+using Snebur.SharedKernel.Localization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+
+namespace Snebur.Infrastructure;
+
+public static class InfrastructureServiceConfiguration
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        IHostEnvironment hostEnvironment)
+    {
+        var azureTranslationSecrets = AzureTranslationSecretsFactory.Create(configuration, hostEnvironment);
+        var localizationConfiguration = JsonLocalizationConfigurationFactory.Create(configuration, hostEnvironment);
+
+        services.AddSingleton<ISecureConfiguration, SecureConfiguration>()
+            .AddSingleton<ICacheRepository, CacheRepository>()
+            .AddSingleton<IFileService, FileService>()
+            .AddSingleton<JsonLocalizationCacheConfiguration>(localizationConfiguration)
+            .AddSingleton(localizationConfiguration)
+            .AddSingleton(azureTranslationSecrets);
+
+        services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var redisConnection = configuration.GetConnectionString("CacheRedis");
+            return ConnectionMultiplexer.Connect(redisConnection!);
+        });
+        services.AddTransient<IEmailSender, EmailSender>();
+
+        return services;
+    }
+}
---
+++ src/Snebur.UseCases/UseCasesServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Snebur.Application;
+using Snebur.UseCases.Identities.Authentications.Services;
+using Snebur.UseCases.Identities.Tenants.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Snebur.UseCases;
+
+public static class UseCasesServiceConfiguration
+{
+    public static IServiceCollection AddUserCasesServices(
+        this IServiceCollection services)
+    {
+        services.AddApplicationHandlersFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddScoped<ITenantValidationService, TenantValidationService>();
+        services.AddScoped<ITenantUserValidationService, TenantUserValidationService>();
+        services.AddScoped<ITenantUserAuthenticationValidationService, TenantUserAuthenticationValidationService>();
+        services.AddScoped<IAdminUserAuthenticationValidationService, AdminUserAuthenticationValidationService>();
+
+        return services;
+    }
+
+}
