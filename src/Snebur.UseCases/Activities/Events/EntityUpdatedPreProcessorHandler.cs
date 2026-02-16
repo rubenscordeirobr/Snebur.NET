68d7368ee8e2b35595d8804b9c03ddf33db7a614
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-16 17:10:01 -0300

    feat: add EntityUpdatedPreProcessorHandler for handling entity update events

---
+++ src/Snebur.UseCases/Activities/Events/EntityUpdatedPreProcessorHandler.cs
@@ -0,0 +1,16 @@
+﻿using Snebur.Domain.Primitives;
+
+namespace Snebur.UseCases.Activities.Events;
+
+public sealed class EntityUpdatedPreProcessorHandler<TEntity>
+    : IEntityUpdatedEventPreProcessorHandler<TEntity>
+    where TEntity : EntityBase
+{
+    public Task PreProcessAsync(
+        IDomainEventData<IEntityUpdatedEvent<TEntity>> eventData, 
+        CancellationToken cancellationToken = default)
+    {
+        //Just for testing purposes
+        return Task.CompletedTask;
+    }
+}
