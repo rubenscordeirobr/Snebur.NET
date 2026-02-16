6375bc05e238c8be4f314d3e1bf1c9047cf6017e
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-16 12:31:29 -0300

    feat(system-users): add query and handler for fetching system user by ID

---
+++ src/Snebur.UseCases.Shared/Identities/Users/SystemUsers/Queries/GetSystemUserByIdQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace Snebur.UseCases.Identities.Users.SystemUsers.Queries;
+
+public record GetSystemUserByIdQuery(Guid Id) : GetEntityByIdQuery<UserResponse>(Id);
---
+++ src/Snebur.UseCases/Identities/Users/SystemUsers/Queries/GetSystemUserByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Snebur.UseCases.Mappers.Identities;
+
+namespace Snebur.UseCases.Identities.Users.SystemUsers.Queries;
+
+public sealed class GetSystemUserByIdQueryHandler
+    : IGetQueryResultHandler<GetSystemUserByIdQuery, UserResponse>
+{
+    private readonly ISystemUserRepository _systemUserRepository;
+    public GetSystemUserByIdQueryHandler(
+        ISystemUserRepository systemUserRepository)
+    {
+        _systemUserRepository = systemUserRepository;
+    }
+
+    public async Task<Result<UserResponse>> HandleAsync(
+        GetSystemUserByIdQuery query,
+        CancellationToken cancellationToken = default)
+    {
+        Guard.NotNull(query);
+
+        var user = await _systemUserRepository.GetByIdAsync(query.Id, cancellationToken);
+        if (user is null)
+        {
+            return Result.NotFoundFailure<UserResponse>(
+                "SystemUser.NotFound",
+                $"SystemUser with id {query.Id} not found.");
+        }
+
+        var userResponse = UserMapper.ToResponse(user);
+        return Result.Success(userResponse);
+    }
+}
