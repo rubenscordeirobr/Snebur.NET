5be6adfbccbd698068bc1a6042d563e31b1a4ecf
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-16 17:18:49 -0300

    feat(users): add handler for fetching all system users

---
+++ src/Snebur.UseCases/Identities/Users/SystemUsers/Queries/GetAllSystemUsersQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Snebur.UseCases.Mappers.Identities;
+
+namespace Snebur.UseCases.Identities.Users.SystemUsers.Queries;
+
+internal sealed partial class GetAllSystemUsersQueryHandler
+    : IGetManyQueryHandler<GetAllSystemUsersQuery, UserResponse>
+{
+    private readonly ISystemUserRepository _systemUserRepository;
+    public GetAllSystemUsersQueryHandler(
+        ISystemUserRepository systemUserRepository)
+    {
+        _systemUserRepository = systemUserRepository;
+    }
+
+    public async Task<Result<IReadOnlyList<UserResponse>>> HandleAsync(
+        GetAllSystemUsersQuery request,
+        CancellationToken cancellationToken = default)
+    {
+        var users = await _systemUserRepository.GetAllAsync(cancellationToken);
+        var userResponses = users.Select(UserMapper.ToResponse).ToList();
+        return this.Success(userResponses);
+    }
+}
