214c6f137d4d2852b76e3a5cfeafa5777272a140
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-18 09:35:42 -0300

    feat: add EmailSender service with SendEmailAsync method

---
+++ src/Snebur.Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using Snebur.Application.Models.Communication;
+
+namespace Snebur.Infrastructure.Services;
+
+public class EmailSender : IEmailSender
+{
+    public async Task<bool> SendEmailAsync(Email email)
+    {
+        //TODO implement email sending
+        await Task.Delay(1000);
+        return true;
+    }
+}
