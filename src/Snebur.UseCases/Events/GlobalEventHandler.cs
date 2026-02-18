55c199c99c1904c9b91d0bc69d98a09325911856
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-18 11:36:05 -0300

    feat: add GlobalEventHandler for domain event handling

---
+++ src/Snebur.UseCases/Events/GlobalEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Snebur.SharedKernel.Abstractions;
+
+namespace Snebur.UseCases.Events;
+
+internal class GlobalEventHandler : IDomainEventHandler<IDomainEvent>
+{
+    private readonly ILogger<GlobalEventHandler> _logger;
+
+    public GlobalEventHandler(ILogger<GlobalEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task HandleAsync(IDomainEvent domainEvent, CancellationToken cancellationToken = default)
+    {
+        //tests propose
+        _logger.LogInformation("Handling event {EventName}", domainEvent.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
