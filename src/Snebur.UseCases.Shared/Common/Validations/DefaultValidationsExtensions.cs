9a7d49c81a035e65a2d21d11fb77e66e874fe535
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-18 09:45:33 -0300

    feat: add default validation extensions for fullname, guid, and sha256

---
+++ src/Snebur.UseCases.Shared/Common/Validations/DefaultValidationsExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Snebur.UseCases.Common.Validations;
+
+public static partial class DefaultValidationsExtensions
+{
+    private static readonly Regex FullNameRegex = new(@"^\p{L}[\p{L}\d'’.\-\s_]*\s+\p{L}[\p{L}\d'’.\-\s_]*$", RegexOptions.Compiled);
+     
+    public static IRuleBuilderOptions<T, string> FullName<T>(
+        this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+            .NotEmpty()
+            .MinimumLength(3)
+            .MaximumLength(100)
+            .Matches(FullNameRegex);
+    }
+
+    //NotEmptyGuid
+    public static IRuleBuilderOptions<T, Guid> NotEmptyGuid<T>(
+        this IRuleBuilder<T, Guid> ruleBuilder)
+    {
+        return ruleBuilder
+            .NotEqual(Guid.Empty);
+    }
+
+    //Sha256
+    public static IRuleBuilderOptions<T, string> Sha256<T>(
+        this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+            .Matches("^[a-f0-9]{64}$");
+    }
+}
