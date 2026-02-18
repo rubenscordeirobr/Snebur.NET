13bda513bb2f45d6ead78c6fd714caa41b38c52a
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-18 09:25:02 -0300

    feat(core): add GlobalUsings file with common namespaces

---
+++ src/Snebur.Core/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Snebur.Core.Extensions;
+global using Snebur.Core.Attributes;
+global using Snebur.Core.Exceptions;
+global using Snebur.Core.Factories;
+global using Snebur.Core.Enums;
+global using Snebur.Core.Helpers;
