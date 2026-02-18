0bdc471ceca87f7dfb68d511cc6c166de6ad1023
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-18 09:46:02 -0300

    feat(validations): add EmailValidValidator for enhanced email validation

---
+++ src/Snebur.UseCases.Shared/Common/Validations/EmailValidValidator.cs
@@ -0,0 +1,35 @@
+﻿using Snebur.Core.Exceptions;
+using Snebur.Core.Utils;
+using FluentValidation.Validators;
+
+namespace Snebur.UseCases.Shared;
+
+public static partial class DefaultValidationsExtensions
+{
+    [Obsolete("Use EmailAddressValid ")]
+    public static IRuleBuilderOptions<T, string> EmailAddress<T>(
+        this IRuleBuilder<T, string> ruleBuilder, 
+        EmailValidationMode mode = EmailValidationMode.AspNetCoreCompatible)
+    {
+        throw new DeprecatedException($"Use {nameof(EmailAddressValid)}");
+    }
+
+    public static IRuleBuilderOptions<T, string> EmailAddressValid<T>(
+     this IRuleBuilder<T, string> ruleBuilder)
+    {
+        Guard.NotNull(ruleBuilder);
+
+        return ruleBuilder.SetValidator(new EmailValidValidator<T>());
+    }
+}
+
+public class EmailValidValidator<T> : PropertyValidator<T, string>, IEmailValidator
+{
+    public override string Name
+        => "EmailValidValidator";
+ 
+    public override bool IsValid(ValidationContext<T> context, string value)
+    {
+        return ValidationUtils.IsEmail(value);
+    }
+}
