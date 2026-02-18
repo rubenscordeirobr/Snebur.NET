76b3bd426ed076b2388c00d78936de9e4a6b1b28
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-18 09:45:10 -0300

    feat(validations): add base CommandValidator class

---
+++ src/Snebur.UseCases.Shared/Common/Validations/CommandValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation.Results;
+namespace Snebur.UseCases.Common.Validations;
+
+public abstract class CommandValidator<TCommand> : AbstractValidator<TCommand>
+    where TCommand : ICommandRequest
+{
+    protected IJsonStringLocalizer<TCommand> Localizer { get; }
+
+    protected CommandValidator(IJsonStringLocalizer<TCommand> localizer)
+    {
+        Localizer = localizer;
+    }
+
+    public override ValidationResult Validate(ValidationContext<TCommand> context)
+    {
+        Guard.NotNull(context);
+
+        var result = base.Validate(context);
+        return NormalizeContext(context, result);
+    }
+
+    public override async Task<ValidationResult> ValidateAsync(
+        ValidationContext<TCommand> context,
+        CancellationToken cancellation = default)
+    {
+        Guard.NotNull(context);
+
+        var result = await base.ValidateAsync(context, cancellation);
+        return NormalizeContext(context, result);
+    }
+
+    private ValidationResult NormalizeContext(
+        ValidationContext<TCommand> context,
+        ValidationResult result)
+    {
+        if (result.IsValid)
+        {
+            context.InstanceToValidate.ValidatedSuccessfullyAt = DateTime.UtcNow;
+        }
+        return result;
+    }
+}
