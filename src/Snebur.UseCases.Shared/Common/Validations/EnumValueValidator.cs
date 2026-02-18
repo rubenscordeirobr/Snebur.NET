86faa1caa413c37a3722f55e6609907472a2f616
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-18 09:46:13 -0300

    feat: add EnumValueValidator for enhanced enum validation

---
+++ src/Snebur.UseCases.Shared/Common/Validations/EnumValueValidator.cs
@@ -0,0 +1,42 @@
+﻿using Snebur.Core.Exceptions;
+using Snebur.Core.Utils;
+using FluentValidation.Validators;
+
+namespace Snebur.UseCases.Common.Validations;
+
+public static partial class DefaultValidationsExtensions
+{
+    [Obsolete("Use IsInEnumValue instead.")]
+    public static IRuleBuilderOptions<T, TProperty> IsInEnum<T, TProperty>(
+        this IRuleBuilder<T, TProperty> ruleBuilder)
+    {
+        throw new DeprecatedException("Use IsInEnumValue instead.");
+    }
+
+    public static IRuleBuilderOptions<T, TProperty> IsInEnumValue<T, TProperty>(
+        this IRuleBuilder<T, TProperty> ruleBuilder)
+        where TProperty : struct, Enum
+    {
+        Guard.NotNull(ruleBuilder);
+
+        return ruleBuilder
+            .SetValidator(new EnumValueValidator<T, TProperty>());
+    }
+}
+
+public class EnumValueValidator<T, TProperty> : EnumValidator<T, TProperty>
+    where TProperty : struct, Enum
+{
+    public override string Name
+        => "EnumValueValidator";
+
+    public override bool IsValid(ValidationContext<T> context, TProperty value)
+    {
+        if (!base.IsValid(context, value))
+        {
+            return false;
+        }
+        return EnumUtils.IsDefined(value);
+
+    }
+}
