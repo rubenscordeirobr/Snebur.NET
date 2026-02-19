aef4e54d94c07d06c4aede0c408a3133b4d52f10
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-19 16:39:05 -0300

    feat: add query and handler for fetching admin user by ID

---
+++ src/Snebur.UseCases.Shared/Identities/Users/AdminUsers/Queries/GetAdminUserByIdQuery.cs
@@ -0,0 +1,4 @@
+﻿namespace Snebur.UseCases.Identities.Users.AdminUsers.Queries;
+
+public record GetAdminUserByIdQuery(Guid Id) 
+    : GetEntityByIdQuery<UserResponse>(Id);
---
+++ src/Snebur.UseCases/Identities/Users/AdminUsers/Queries/GetAdminUserByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Snebur.UseCases.Mappers.Identities;
+
+namespace Snebur.UseCases.Identities.Users.AdminUsers.Queries;
+
+public class GetAdminUserByIdQueryHandler
+     : IGetQueryResultHandler<GetAdminUserByIdQuery, UserResponse>
+{
+    private readonly IAdminUserRepository _adminUserRepository;
+    public GetAdminUserByIdQueryHandler(
+        IAdminUserRepository adminUserRepository)
+    {
+        _adminUserRepository = adminUserRepository;
+
+    }
+    public async Task<Result<UserResponse>> HandleAsync(
+        GetAdminUserByIdQuery query,
+        CancellationToken cancellationToken = default)
+    {
+        Guard.NotNull(query);
+
+        var user = await _adminUserRepository.GetByIdAsync(query.Id, cancellationToken);
+        if (user is null)
+        {
+            return Result.NotFoundFailure<UserResponse>(
+                "SystemUser.NotFound",
+                $"SystemUser with id {query.Id} not found.");
+        }
+
+        var userResponse = UserMapper.ToResponse(user);
+        return Result.Success(userResponse);
+    }
+}
+
