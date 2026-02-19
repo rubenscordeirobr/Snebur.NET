2e07eebf2211beae07668c1f2c3c3f8d3f5226d0
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-19 11:33:02 -0300

    feat(presentation): add initial setup for Snebur.Presentation project

---
+++ src/Snebur.Presentation/PresentationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Snebur.Presentation.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace Snebur.Presentation;
+
+public static class PresentationExtensions
+{
+    public static IApplicationBuilder UsePresentationServices(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<SessionVerificationMiddleware>();
+        return app;
+    }
+}
+
---
+++ src/Snebur.Presentation/PresentationServiceConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Snebur.Application.Extensions;
+using Snebur.Presentation.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Snebur.Presentation;
+
+public static class PresentationServiceConfiguration
+{
+    public static IServiceCollection AddPresentationServices(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IHttpContextSessionAccessor, HttpContextSessionAccessor>()
+            .AddTransient(provider =>
+            {
+                var accessor = provider.GetRequiredService<IHttpContextSessionAccessor>();
+                return accessor.GetRequiredUserSession();
+            });
+        return services;
+    }
+}
