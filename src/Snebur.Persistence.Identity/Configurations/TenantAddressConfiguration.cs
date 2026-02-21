556f4e0b0841fb7fda0e44d0e74e84e35d345b86
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-21 12:02:08 -0300

    feat: add TenantAddress entity and configuration

---
+++ src/Snebur.Persistence.Identity/Configurations/TenantAddressConfiguration.cs
@@ -0,0 +1,42 @@
+﻿namespace Snebur.Persistence.Identity.Configurations;
+
+public class TenantAddressConfiguration : IEntityTypeConfiguration<TenantAddress>
+{
+    public void Configure(EntityTypeBuilder<TenantAddress> builder)
+    {
+        Guard.NotNull(builder);
+
+        builder.Property(x => x.AddressName)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.AddressNameMaxLength);
+
+        builder.Property(x => x.Street)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.StreetMaxLength);
+
+        builder.Property(x => x.Number)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.AddressNumberMaxLength);
+
+        builder.Property(x => x.Complement)
+            .IsRequired(false)
+            .HasMaxLength(ValidationConstants.AddressComplementMaxLength);
+
+        builder.Property(x => x.Neighborhood)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.NeighborhoodMaxLength);
+
+        builder.Property(x => x.City)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.CityMaxLength);
+
+        builder.Property(x => x.State)
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.AddressStateMaxLength);
+  
+        builder.Property(x => x.ZipCode)
+
+            .IsRequired()
+            .HasMaxLength(ValidationConstants.ZipCodeMaxLength);
+    }
+}
