4da92a3e617984e8d44c3d6fb66cdec066315c77
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-23 08:34:09 -0300

    feat: add extension methods for setting property values and managing user session attributes

---
+++ src/Snebur.Persistence.Identity/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using FluentValidation.Internal;
+
+namespace Snebur.Persistence.Identity.Extensions;
+
+internal static class ExpressionExtensions
+{
+    internal static void SetPropertyValue<T, TProperty>(this T user,
+       Expression<Func<T, TProperty>> expression, TProperty value)
+    {
+        var memberInfo = expression.GetMember()
+            ?? throw new InvalidOperationException("Invalid member expression.");
+
+        if (memberInfo is not PropertyInfo propertyInfo)
+        {
+            throw new InvalidOperationException("Member is not a property.");
+        }
+
+        if (propertyInfo.SetMethod == null)
+        {
+            throw new InvalidOperationException("Property is read-only.");
+        }
+        propertyInfo.SetValue(user, value);
+    }
+}
---
+++ src/Snebur.Persistence.Identity/Extensions/UserSessionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Snebur.Persistence.Identity.Extensions;
+
+internal static class UserSessionExtensions
+{
+    internal static void SetAnonymousSystemSessionId(this UserSession session)
+    {
+        if (session.Id != Guid.Empty)
+        {
+            throw new InvalidOperationException("Id is already set.");
+        }
+                
+        session.SetPropertyValue(p => p.Id, AnonymousUserConstants.Session_Id);
+        session.SetPropertyValue(p => p.AuthenticationType, AuthenticationType.Anonymous);
+        session.SetPropertyValue(p => p.Tenant_Id, null);
+        session.SetCreateSession(AnonymousUserConstants.Session_Id);
+    }
+}
