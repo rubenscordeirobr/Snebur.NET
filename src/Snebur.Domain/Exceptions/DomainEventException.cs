9cc8c8bdec8a1f6e5231488151545a66a2438ffe
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-23 10:17:06 -0300

    feat: add DomainEventException class

---
+++ src/Snebur.Domain/Exceptions/DomainEventException.cs
@@ -0,0 +1,9 @@
+﻿namespace Snebur.Domain.Exceptions;
+
+public class DomainEventException : DomainException
+{
+    public DomainEventException(string message)
+        : base(message)
+    {
+    }
+}
