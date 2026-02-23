3f5449421fdc1128a71aadbe07c8924b430a3129
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-23 10:29:54 -0300

    feat(tests): add extensions for ServiceDescriptor and TestResult

---
+++ tests/Snebur.ArchitectureTests/Extensions/ServiceDescriptorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Snebur.ArchitectureTests.Extensions;
+
+public static class ServiceDescriptorExtensions
+{
+    public static bool IsAssignableTo(
+        this ServiceDescriptor descriptor,
+        IEnumerable<Type> targetTypes)
+    {
+        return descriptor.ServiceType?.IsAssignableTo(targetTypes) == true ||
+                descriptor.ImplementationType?.IsAssignableTo(targetTypes) == true;
+    }
+}
+
---
+++ tests/Snebur.ArchitectureTests/Extensions/TestResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Snebur.ArchitectureTests.Extensions;
+
+public static class TestResultExtensions
+{
+    public static string GetFailingTypeNames(this NetArchTest.Rules.TestResult result)
+    {
+        if (result.FailingTypeNames is null)
+            return string.Empty;
+
+        return string.Join(",", result.FailingTypeNames);
+    }
+}
