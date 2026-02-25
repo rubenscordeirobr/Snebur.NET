0adc7026af84b1ef06ffbd745de4c729a9722618
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-25 10:23:56 -0300

    feat: add ApiEndpointBase and ResponseResult to handle API requests and responses

---
+++ src/Snebur.Presentation/Common/ApiEndpointBase.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using Snebur.SharedKernel.Abstractions;
+
+namespace Snebur.Presentation.Common;
+
+public abstract class ApiEndpointBase : IEndpointService
+{
+    private readonly ServiceRole _serviceRole;
+
+    public virtual ServiceRole ServiceRole
+        => _serviceRole;
+
+    public virtual string ServiceName
+         => this.GetType().Name;
+
+    protected ApiEndpointBase()
+    {
+        _serviceRole = GetSessionRole();
+    }
+     
+    internal virtual async Task<ResponseResult> ProcessAsync(
+        HttpMethodDescriptor descriptor,
+        object?[]? parameterValues)
+    {
+        var method = descriptor.Method;
+        var methodResult = method.Invoke(
+           this,
+           parameterValues);
+
+        var successStatusCode = descriptor.SuccessStatusCode;
+        if (methodResult is not Task task)
+        {
+            return ResponseResult.SuccessWithStatus(successStatusCode, methodResult!);
+        }
+
+        await task.ConfigureAwait(false);
+
+        var taskResult = task.GetResult();
+        if (taskResult is not IResultValue resultValue)
+        {
+            return ResponseResult.SuccessWithStatus(successStatusCode, taskResult!);
+        }
+
+        if (resultValue.IsSuccess)
+        {
+            return ResponseResult.SuccessWithStatus(successStatusCode, resultValue.Value!);
+        }
+        return ResponseResult.Error(resultValue.Error);
+    }
+
+    public virtual JsonSerializerOptions? GetJsonSerializerOptions()
+    {
+        return null;
+    }
+
+    private ServiceRole GetSessionRole()
+    {
+        var attribute = GetType().GetCustomAttribute<ServiceRoleAttribute>();
+        return attribute?.ServiceRole ?? ServiceRole.Default;
+    }
+}
+
---
+++ src/Snebur.Presentation/Common/ResponseResult.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using Snebur.Core.Enums;
+
+namespace Snebur.Presentation.Common;
+
+internal sealed record ResponseResult
+{
+    [MemberNotNullWhen(false, nameof(ErrorResponse))]
+    public bool IsSuccess { get; }
+    public int StatusCode { get; }
+    public object? Value { get; }
+
+    public ErrorResponse? ErrorResponse { get; }
+
+    private ResponseResult(HttpStatusCode statusCode, object value)
+    {
+        Value = value;
+        StatusCode = (int)statusCode;
+        IsSuccess = true;
+    }
+
+    private ResponseResult(HttpStatusCode statusCode, string errorCode, string errorMessage)
+    {
+        ErrorResponse = new ErrorResponse(errorCode, errorMessage);
+        StatusCode = (int)statusCode;
+        IsSuccess = false;
+    }
+
+    private ResponseResult(HttpStatusCode statusCode, ErrorResponse errorResponse)
+    {
+        ErrorResponse = errorResponse;
+        StatusCode = (int)statusCode;
+        IsSuccess = false;
+    }
+
+    private ResponseResult(ExtendedHttpStatusCode statusCode, string errorCode, string errorMessage)
+    {
+        ErrorResponse = new ErrorResponse(errorCode, errorMessage);
+        StatusCode = (int)statusCode;
+        IsSuccess = false;
+    }
+    public static ResponseResult Ok(object response)
+        => new ResponseResult(HttpStatusCode.OK, response);
+
+    public static ResponseResult SuccessWithStatus(
+        HttpStatusCode statusCode,
+        object response)
+        => new ResponseResult(statusCode, response);
+
+    public static ResponseResult Error(
+        HttpStatusCode statusCode,
+        string errorCode,
+        string errorMessage)
+        => new ResponseResult(statusCode, errorCode, errorMessage);
+
+    public static ResponseResult Error(
+        ExtendedHttpStatusCode statusCode,
+        string errorCode,
+        string errorMessage)
+        => new ResponseResult(statusCode, errorCode, errorMessage);
+
+    public static ResponseResult Error(Error error)
+        => new ResponseResult(error.StatusCode, error.CreateErrorResponse());
+}
