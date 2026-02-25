83e38fd5aeca09baf783a699c52f7b5c46b7b21c
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-25 07:50:22 -0300

    feat: add ExceptionExtensions with GetNestedMessage method and unit tests

---
+++ tests/Snebur.Core.UnitTests/Extensions/ExceptionExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Snebur.Core.UnitTests.Extensions;
+
+public class ExceptionExtensionsTests
+{
+    [Fact]
+    public void GetNestedMessage_ShouldReturnSingleMessage_WhenExceptionHasNoInnerException()
+    {
+        // Arrange
+        var exception = new Exception("Test exception");
+
+        // Act
+        var result = exception.GetNestedMessage();
+
+        // Assert
+        result.Should().Be("Test exception");
+    }
+
+    [Fact]
+    public void GetNestedMessage_ShouldReturnNestedMessages_WhenExceptionHasInnerExceptions()
+    {
+        // Arrange
+        var innerException = new Exception("Inner exception");
+        var exception = new Exception("Outer exception", innerException);
+
+        // Act
+        var result = exception.GetNestedMessage();
+
+        // Assert
+        result.Should().Be("Outer exception -> Inner exception");
+    }
+
+    [Fact]
+    public void GetNestedMessage_ShouldThrowArgumentNullException_WhenExceptionIsNull()
+    {
+        // Arrange
+        Exception? exception = null;
+
+        // Act
+        Action act = () => exception!.GetNestedMessage();
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+}
+
