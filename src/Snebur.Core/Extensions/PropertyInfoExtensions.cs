0b062b9203dd6529778221d02cd3198396cc8486
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-02-25 07:13:28 -0300

    feat: add GetPropertyPath extension method for PropertyInfo

---
+++ src/Snebur.Core/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace Snebur.Core.Extensions;
+
+public static class PropertyInfoExtensions
+{
+    public static string GetPropertyPath(this PropertyInfo propertyInfo)
+    {
+        Guard.NotNull(propertyInfo);
+
+        return $"{propertyInfo.DeclaringType?.GetQualifiedName()}::{propertyInfo.Name}";
+    }
+}
---
+++ tests/Snebur.Core.UnitTests/Extensions/PropertyInfoExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Snebur.Core.UnitTests.Extensions;
+public class PropertyInfoExtensionsTests
+{
+    private class NestedPropertyTestClass
+    {
+        public string? TestProperty { get; set; }
+    }
+
+    [Theory]
+    [InlineData(typeof(NestedPropertyTestClass), nameof(NestedPropertyTestClass.TestProperty), "Snebur.Core.UnitTests.Extensions.PropertyInfoExtensionsTests+NestedPropertyTestClass::TestProperty")]
+    [InlineData(typeof(TestPropertyPathClass), nameof(TestPropertyPathClass.TestProperty), "Snebur.Core.UnitTests.Extensions.TestPropertyPathClass::TestProperty")]
+    public void GetPropertyPath_ShouldReturnCorrectPath(Type type, string propertyName, string result)
+    {
+        // Arrange
+        var propertyInfo = type.GetProperty(propertyName);
+
+        // Act
+        var propertyPath = propertyInfo?.GetPropertyPath();
+
+        // Assert
+        propertyPath.Should().Be(result);
+    }
+}
+
+internal class TestPropertyPathClass
+{
+    public string? TestProperty { get; set; }
+}
