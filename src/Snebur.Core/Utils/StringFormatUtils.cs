b82eae61e0f05dfc3f7458540e1193d53511fd8e
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-09 13:08:19 -0300

    feat: add StringFormatUtils with format method and unit tests

---
+++ src/Snebur.Core/Utils/StringFormatUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Snebur.Core.Utils;
+
+public static class StringFormatUtils
+{
+    private static readonly Regex PlaceholderRegex = new(@"\{(\w+)\}", RegexOptions.Compiled);
+
+    public static string Format(string format, params object[] args)
+    {
+        Guard.NotNull(format);
+
+        if (args?.Length > 0)
+        {
+            return PlaceholderRegex.Replace(format, match =>
+            {
+                var index = Math.Min(args.Length - 1, match.Index);
+                return args[index].ToString() ?? string.Empty;
+            });
+        }
+        return format;
+    }
+}
+
---
+++ tests/Snebur.Core.UnitTests/Utils/StringFormatUtilsTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Snebur.Core.UnitTests.Utils;
+
+public class StringFormatUtilsTests
+{
+    [Fact]
+    public void Format_NoArgs_ShouldReturnSameString()
+    {
+        // Arrange
+        var input = "Hello world";
+
+        // Act
+        var result = StringFormatUtils.Format(input);
+
+        // Assert
+        result.Should().Be(input);
+    }
+
+    [Fact]
+    public void Format_WithSingleArg_ShouldReplacePlaceholder()
+    {
+        // Arrange
+        var input = "Hello {name}";
+        // Here, the regex replaces the placeholder using the argument at index = Math.Min(args.Length-1, match.Index).
+        // For this input, match.Index will be >= 0 so it will always select args[0].
+        var expected = "Hello World";
+
+        // Act
+        var result = StringFormatUtils.Format(input, "World");
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Format_WithMultiplePlaceholders_ShouldReplaceAllWithSameArgWhenIndexExceeds()
+    {
+        // Arrange
+        var input = "Value: {a} and {b}";
+        // With two arguments, the replacement for both placeholders will use the argument at index:
+        // Math.Min(1, match.Index). Given match.Index of both placeholders is >= 1, both will be replaced by args[1] ("Second")
+        var expected = "Value: Second and Second";
+
+        // Act
+        var result = StringFormatUtils.Format(input, "First", "Second");
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Format_NullFormat_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        Action act = () => StringFormatUtils.Format(null!, "test");
+
+        // Act & Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+}
+
