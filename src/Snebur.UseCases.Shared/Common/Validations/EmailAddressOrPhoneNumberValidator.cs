f096569bb46e5dfb234a5cf876666099cebbfadb
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-09 13:11:56 -0300

    feat: add EmailAddressOrPhoneNumberValidator for combined validation

---
+++ src/Snebur.UseCases.Shared/Common/Validations/EmailAddressOrPhoneNumberValidator.cs
@@ -0,0 +1,28 @@
+﻿using Snebur.Core.Utils;
+using FluentValidation.Validators;
+
+namespace Snebur.UseCases.Common.Validations;
+
+public static partial class DefaultValidationsExtensions
+{
+    public static IRuleBuilderOptions<T, string> EmailAddressOrPhoneNumber<T>(
+        this IRuleBuilder<T, string> ruleBuilder)
+    {
+        Guard.NotNull(ruleBuilder);
+
+        return ruleBuilder.SetValidator(new EmailValidValidator<T>());
+    }
+}
+
+public class EmailAddressOrPhoneNumberValidator<T> : PropertyValidator<T, string>, IEmailValidator
+{
+    public override string Name
+        => "EmailAddressOrPhoneNumberValidator";
+
+    public override bool IsValid(ValidationContext<T> context, string value)
+    {
+        return ValidationUtils.IsEmail(value) ||
+            ValidationUtils.IsFullPhoneNumberValid(value);
+    }
+}
+
