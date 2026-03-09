0ab8e7216ae4459e0fe7fb64ef6e630a47228a38
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-09 13:46:17 -0300

    feat: add MaxAuthenticationResult model to handle max authentication logic

---
+++ src/Snebur.Application/Models/Security/MaxAuthenticationResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Snebur.Application.Models.Security;
+public partial record MaxAuthenticationResult(
+    bool IsMaxReached, 
+    int CurrentAttempts,
+    TimeSpan? ExpirationTime = null);
+
+public partial record MaxAuthenticationResult
+{
+    public static MaxAuthenticationResult Success
+        => new MaxAuthenticationResult(false, 0);
+}
+
