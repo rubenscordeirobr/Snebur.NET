b32771fbd0de96ce4ecdc5d12f9009821f818692
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-09 13:52:22 -0300

    feat: add UserSessionTerminatedEventHandler implementation

---
+++ src/Snebur.UseCases/Identities/UserSessions/Events/UserSessionTerminatedEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using Snebur.Domain.Entities.Identities.Events;
+
+namespace Snebur.UseCases.Identities.UserSessions.Events;
+
+internal class UserSessionTerminatedEventHandler : IDomainEventHandler<UserSessionTerminatedEvent>
+{
+    private readonly ILogger<UserSessionTerminatedEventHandler> _logger;
+
+    public UserSessionTerminatedEventHandler(ILogger<UserSessionTerminatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task HandleAsync(
+        UserSessionTerminatedEvent domainEvent,
+        CancellationToken cancellationToken = default)
+    {
+        //TODO: disconnect user session
+        _logger.LogInformation(
+            "User session {UserSessionId} terminated. Reason: {Reason}",
+            domainEvent.UserSession.Id,
+            domainEvent.Reason);
+
+        return Task.CompletedTask;
+    }
+}
