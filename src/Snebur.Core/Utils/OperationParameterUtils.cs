739d3c8e9d1bd38d7a5886e4a9fcfdfeb75a9827
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-15 19:12:59 -0300

    feat(utils): add `NormalizeKey` method to OperationParameterUtils and unit tests

---
+++ src/Snebur.Core/Utils/OperationParameterUtils.cs
@@ -0,0 +1,20 @@
+﻿namespace Snebur.Core.Utils;
+
+public static class OperationParameterUtils
+{
+    public static string NormalizeKey(string? key)
+    {
+        Guard.NotNullOrWhiteSpace(key);
+
+        var index = 0;
+        while (char.IsUpper(key[index]))
+        {
+            index++;
+        }
+        if (index > 0)
+        {
+            return string.Concat(key[..index].ToLowerInvariant(), key.AsSpan(index));
+        }
+        return key;
+    }
+}
---
+++ tests/Snebur.Core.UnitTests/Utils/OperationParameterUtilsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Snebur.Core.UnitTests.Utils;
+
+public class OperationParameterUtilsTests
+{
+    [Theory]
+    [InlineData("ABCdef", "abcdef")]
+    [InlineData("ABCdEf", "abcdEf")]
+    [InlineData("Abcdef", "abcdef")]
+    [InlineData("abcdef", "abcdef")]
+    public void NormalizeKey_Should_Transform_Correctly(string input, string expected)
+    {
+        // Act
+        var result = OperationParameterUtils.NormalizeKey(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void NormalizeKey_Should_ThrowException_When_Input_Is_NullOrWhiteSpace(string? input)
+    {
+        // Act
+        Action act = () => OperationParameterUtils.NormalizeKey(input);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+}
+
