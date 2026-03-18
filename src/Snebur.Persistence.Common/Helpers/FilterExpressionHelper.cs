d24bf849401126dbf3ea8f1cde0918e18b1b8536
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-18 08:54:15 -0300

    `feat: add FilterExpressionHelper with soft delete filter functionality`

---
+++ src/Snebur.Persistence.Common/Helpers/FilterExpressionHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Snebur.Persistence.Common.Helpers;
+
+internal static class FilterExpressionHelper
+{
+    internal static string AppendSoftDeleteFilter(
+        string? currentFilter,
+        string deletedColumnName)
+    {
+        var deletedFilter = $"{deletedColumnName} = false";
+        if (string.IsNullOrEmpty(currentFilter))
+        {
+            return deletedFilter;
+        }
+
+        if(currentFilter.Contains(deletedColumnName, StringComparison.InvariantCultureIgnoreCase))
+        {
+            return currentFilter;
+        }
+        return $"{currentFilter} AND {deletedFilter}";
+    }
+}
+
