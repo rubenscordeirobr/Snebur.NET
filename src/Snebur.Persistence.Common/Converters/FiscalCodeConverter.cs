af93c6cc5604d16cd72621d1001a22776b0bcdf3
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-19 09:40:58 -0300

    feat: add FiscalCode value object and EF core converter

---
+++ src/Snebur.Persistence.Common/Converters/FiscalCodeConverter.cs
@@ -0,0 +1,15 @@
+﻿using Snebur.SharedKernel.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Snebur.Persistence.Common.Converters;
+
+internal class FiscalCodeConverter : ValueConverter<FiscalCode, string>
+{
+    private FiscalCodeConverter() : base(
+        fiscalCode => fiscalCode.Value,
+        value => new FiscalCode(value))
+    {
+    }
+
+    internal static FiscalCodeConverter Instance { get; } = new();
+}
