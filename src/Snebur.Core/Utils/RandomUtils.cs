f74a21a216cbe0eef0688605513642ee9125ccb2
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-20 08:48:34 -0300

    feat: add RandomUtils with number generation and unit tests

---
+++ src/Snebur.Core/Utils/RandomUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace Snebur.Core.Utils;
+
+public static class RandomUtils
+{
+    private static readonly Random _random = new();
+
+    
+    public static string GenerateRandomNumber(int length)
+    {
+        var digits = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            digits[i] = (char)('0' + _random.Next(0, 10));
+        }
+        return new string(digits);
+    }
+}
---
+++ tests/Snebur.Core.UnitTests/Utils/RandomUtilsTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Snebur.Core.UnitTests.Utils;
+
+public static class RandomUtilsTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(5)]
+    [InlineData(10)]
+    public static void GenerateRandomNumber_Should_ReturnStringOfSpecifiedLength(int length)
+    {
+        // Act
+        var result = RandomUtils.GenerateRandomNumber(length);
+
+        // Assert
+        result.Should().HaveLength(length);
+    }
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+    [InlineData(15)]
+    public static void GenerateRandomNumber_Should_ContainOnlyDigits(int length)
+    {
+        // Act
+        var result = RandomUtils.GenerateRandomNumber(length);
+
+        // Assert
+        foreach (var c in result)
+        {
+            char.IsDigit(c).Should().BeTrue();
+        }
+    }
+}
+
