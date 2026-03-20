c1103c1b1aa4da50cd7c43d66b78d962a284c5e6
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-20 08:57:06 -0300

    feat(mappers): add AddressMapper for mapping TenantAddress to AddressDto

---
+++ src/Snebur.UseCases/Mappers/AddressMapper.cs
@@ -0,0 +1,27 @@
+﻿using Snebur.UseCases.Shared;
+
+namespace Snebur.UseCases.Mappers;
+
+public static class AddressMapper
+{
+    internal static AddressDto? ToDto(TenantAddress? defaultAddress)
+    {
+        if (defaultAddress is null)
+        {
+            return null;
+        }
+
+        return new AddressDto
+        {
+            AddressName = defaultAddress.AddressName,
+            Street = defaultAddress.Street,
+            Number = defaultAddress.Number,
+            Complement = defaultAddress.Complement,
+            Neighborhood = defaultAddress.Neighborhood,
+            City = defaultAddress.City,
+            State = defaultAddress.State,
+            ZipCode = defaultAddress.ZipCode,
+            Country = defaultAddress.Country
+        };
+    }
+}
