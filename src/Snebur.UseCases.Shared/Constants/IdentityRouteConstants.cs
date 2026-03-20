ba68a22c256a628becbc261fe44d851871c28450
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-20 08:57:32 -0300

    feat: add route constants for identity and general API paths

---
+++ src/Snebur.UseCases.Shared/Constants/IdentityRouteConstants.cs
@@ -0,0 +1,28 @@
+﻿namespace Snebur.UseCases.Constants;
+
+ 
+    public static class IdentityRouteConstants
+{
+    private const string IdentityBase = $"{RouteConstants.Api}/identity";
+     
+    public const string Tenants = $"{IdentityBase}/tenants";
+    
+    public const string TenantValidation = $"{IdentityBase}/tenant-validation";
+    public const string TenantUserValidation = $"{IdentityBase}/tenant-users-validation";
+    public const string TenantUserAuthentication = $"{IdentityBase}/tenant-user-authentication";
+    public const string TenantUserAuthenticationValidation = $"{IdentityBase}/tenant-user-authentication-validation";
+
+    public const string AdminUserAuthentication = $"{IdentityBase}/admin-user-authentication";
+    public const string AdminUserAuthenticationValidation = $"{IdentityBase}/admin-user-authentication-validation";
+
+    public const string TenantAddress = $"{IdentityBase}/tenant-address";
+    public const string TenantUpdateDefaultAddress = "update-default-address";
+
+    public const string TenantUsersRoute = $"{IdentityBase}/tenant-users";
+    public const string AdminUsersRoute = $"{IdentityBase}/admin-users";
+
+    //TenantUserAuthenticationService
+    public const string Login = "login";
+    public const string Logout = "logout";
+    public const string CreateTenantAccount = "create-tenant-account";
+}
---
+++ src/Snebur.UseCases.Shared/Constants/RouteConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Snebur.UseCases.Constants;
+
+public static class RouteConstants
+{
+    public const string RouteId = "{id}";
+    public const string Api = "api";
+    public const string Version = "Version";
+    public const string StringLocalizerService = "localization";
+}
