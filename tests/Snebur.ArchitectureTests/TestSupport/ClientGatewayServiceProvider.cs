d0f05eb64d1fbab260039b16036c114c362322d5
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-20 08:53:45 -0300

    feat(tests): add ClientGatewayServiceProvider for test support

---
+++ tests/Snebur.ArchitectureTests/TestSupport/ClientGatewayServiceProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Snebur.ArchitectureTests.TestSupport;
+
+public class ClientGatewayServiceProvider : AbstractTestOutputServiceProvider
+{
+    private readonly IServiceProvider _serviceProvider;
+    
+    public IServiceCollection Services { get; }
+    protected override IServiceProvider ServiceProvider
+        => _serviceProvider;
+
+    public ClientGatewayServiceProvider()
+    {
+        Services = new ClientGatewayServiceCollection();
+        _serviceProvider = Services.BuildServiceProvider();
+    }
+}
