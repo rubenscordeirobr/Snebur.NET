b86268f889c8af7fef31a565c23ccb4a19380dd1
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-22 18:49:59 -0300

    feat(attributes): add HttpDeleteAttribute and HttpPatchAttribute classes

---
+++ src/Snebur.Presentation/Common/Attributes/HttpDeleteAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Snebur.Presentation.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+public sealed class HttpDeleteAttribute : HttpMethodAttribute
+{
+    public override HttpVerb HttpVerb
+        => HttpVerb.Delete;
+    public HttpDeleteAttribute(string routeTemplate = "") : base(routeTemplate) { }
+
+    public HttpDeleteAttribute(HttpStatusCode successStatusCode, string routeTemplate = "")
+        : base(successStatusCode, routeTemplate) { }
+}
---
+++ src/Snebur.Presentation/Common/Attributes/HttpPatchAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Snebur.Presentation.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+public sealed class HttpPatchAttribute : HttpMethodAttribute
+{
+    public override HttpVerb HttpVerb
+        => HttpVerb.Patch;
+
+    public HttpPatchAttribute(string routeTemplate = "") : base(routeTemplate) { }
+
+    public HttpPatchAttribute(HttpStatusCode successStatusCode, string routeTemplate = "")
+        : base(successStatusCode, routeTemplate) { }
+}
