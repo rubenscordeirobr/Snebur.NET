6051547a26da125317118c772e68c38231db14d3
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-22 18:56:51 -0300

    chore: add GlobalSuppressions.cs with suppression for deprecated code rule

---
+++ src/Snebur.UseCases.Shared/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Info Code Smell",
+    "S1133:Deprecated code should be removed", 
+    Justification = "The obsolete attribute is used to avoid obsolete validation from FluentValidation ",
+    Scope = "module")]
