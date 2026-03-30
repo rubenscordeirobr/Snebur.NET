fdbb3325f6bda9abbd841a8ee6b7bc2389f952fa
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-30 16:34:39 -0300

    feat(identity): add API and tenant address endpoints

---
+++ src/Snebur.Presentation/Endpoints/Identity/ApiEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using Snebur.UseCases.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Snebur.Presentation.Endpoints.Identity;
+
+[AllowAnonymous]
+[EndPoint(RouteConstants.Api)]
+public class ApiEndpoint : ApiEndpointBase, IApiService
+{
+    [HttpGet(routeTemplate: RouteConstants.Version)]
+    public string GetVersion()
+    {
+        return "0.0.1";
+    }
+}
+
---
+++ src/Snebur.Presentation/Endpoints/Identity/TenantAddressService.cs
@@ -0,0 +1,22 @@
+﻿using Snebur.UseCases.Identities.Tenants.Commands;
+
+namespace Snebur.Presentation.Endpoints.Identity;
+
+[EndPoint(IdentityRouteConstants.TenantAddress)]
+public class TenantAddressService : ApiEndpointBase, ITenantAddressService
+{
+    private readonly IRequestMediator _mediator;
+
+    public TenantAddressService(IRequestMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost(IdentityRouteConstants.TenantUpdateDefaultAddress)]
+    public Task<Result<OperationResponse>> UpdateDefaultAddressAsync(
+         UpdateDefaultTenantAddressCommand command,
+         CancellationToken cancellationToken = default)
+    {
+        return _mediator.RunAsync(command, cancellationToken);
+    }
+}
