e37c3311b14543203dab12eb9c6dd0a19328dfa6
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-30 15:28:44 -0300

    `feat(identity-api): add initial project setup with Dockerfile and configuration files`

---
+++ src/Snebur.IdentityApi/Program.Testing.cs
@@ -0,0 +1,9 @@
+﻿// Require a public Program class to implement the
+// fixture for the WebApplicationFactory in the
+// integration tests.
+
+#pragma warning disable S1118, S3903 
+public partial class Program
+{
+}
+#pragma warning restore S1118
---
+++ src/Snebur.IdentityApi/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace Snebur.IdentityApi;
+
+internal static class WebApplicationBuilderExtensions
+{
+    internal static IHostApplicationBuilder InitializeEnvironmentSettings(
+        this IHostApplicationBuilder builder)
+    {
+        var baseEnv = builder.Environment.EnvironmentName;
+        var subEnv = Environment.GetEnvironmentVariable("ASPNETCORE_SUB_ENVIRONMENT");
+        var envConfig = string.IsNullOrEmpty(subEnv) ? baseEnv : $"{baseEnv}.{subEnv}";
+
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{baseEnv}.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{envConfig}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        return builder;
+    }
+
+    public static IHostApplicationBuilder ConfigureCors(
+         this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddCors(OptionsBuilderConfigurationExtensions =>
+        {
+            OptionsBuilderConfigurationExtensions.AddPolicy("all", policy =>
+            {
+                policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+        return builder;
+    }
+    public static IHostApplicationBuilder AddEssentialServices(
+         this IHostApplicationBuilder builder)
+    {
+#pragma warning disable S125 
+        /* builder.AddServiceDefaults(); */
+#pragma warning restore S125 
+        builder.Services
+          .AddHttpContextAccessor()
+          .AddOpenApi()
+          .AddSwaggerGen();
+         
+        builder.Services.AddControllers(options =>
+        {
+            options.Conventions.Add(new RouteTokenTransformerConvention(new KebabCaseTransformer()));
+        });
+        return builder;
+    }
+}
