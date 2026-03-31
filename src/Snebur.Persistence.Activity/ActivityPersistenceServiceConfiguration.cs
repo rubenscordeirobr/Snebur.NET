fbb173d524ce034c23cc224eae73fab9c56ec40d
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-31 10:17:16 +0200

    feat: add ActivityPersistenceServiceConfiguration for MongoDB setup

---
+++ src/Snebur.Persistence.Activity/ActivityPersistenceServiceConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Snebur.Application.Abstractions.Persistence.Activities;
+using Snebur.Persistence.Activity.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace Snebur.Persistence.Activity;
+
+public static class ActivityPersistenceServiceConfiguration
+{
+    public static IServiceCollection AddActivityPersistenceServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSingleton<IMongoClient>(provider =>
+        {
+            var connectionString = configuration.GetConnectionString("ActivityMongoDb");
+            // Apply global convention to ignore nulls
+            var conventionPack = new ConventionPack
+            {
+                new IgnoreIfNullConvention(true)
+            };
+            ConventionRegistry.Register("IgnoreNulls", conventionPack, _ => true);
+
+            return new MongoClient(connectionString);
+        });
+
+        services.AddScoped(provider =>
+        {
+            var client = provider.GetRequiredService<IMongoClient>();
+            return client.GetDatabase("activityDB");
+        });
+
+        services.AddScoped<IActivityRepository, ActivityRepository>();
+        return services;
+    }
+}
