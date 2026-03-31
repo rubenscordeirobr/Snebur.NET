3b1ef7251fd208aba8c37507cbf30f7cdc98b915
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-03-31 08:31:20 +0200

    feat(application): add EventMediatorExtensions for domain event dispatching

---
+++ src/Snebur.Application/Extensions/EventMediatorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Snebur.Application.Abstractions.Mediators;
+using Snebur.Application.Events;
+
+namespace Snebur.Application.Extensions;
+
+public static class EventMediatorExtensions
+{
+    public static Task DispatchAsync(this IEventMediator mediator,
+        IUserSession userSession,
+        IDomainEvent domainEvent)
+    {
+        Guard.NotNull(mediator);
+        Guard.NotNull(domainEvent);
+
+        var domainEventContext = new DomainEventContext(
+            userSession,
+            [domainEvent]);
+
+        return mediator.DispatchAsync(domainEventContext);
+    }
+}
+
