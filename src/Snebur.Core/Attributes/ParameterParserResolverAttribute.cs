441aa4d4186ec1b2ad11e008f848c68812219dee
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-06 19:38:39 +0200

    feat: add ParameterParserResolverAttribute for parameter parsing customization

---
+++ src/Snebur.Core/Attributes/ParameterParserResolverAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace Snebur.Core.Attributes;
+
+[AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
+public abstract class ParameterParserResolverAttribute : Attribute
+{
+    public abstract object? Parse(string? stringValue);
+}
+
+[AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
+public sealed class ParameterParserResolverAttribute<TResolver> : ParameterParserResolverAttribute
+    where TResolver : IParameterParserResolver, new()
+{
+    private readonly TResolver _resolverInstance;
+   
+    public ParameterParserResolverAttribute()
+    {
+        _resolverInstance = new TResolver();
+    }
+
+    public override object? Parse(string? stringValue)
+    {
+        if (_resolverInstance is null)
+        {
+            throw new InvalidOperationException(
+                $"Provider instance of type '{typeof(TResolver).Name}' is null.");
+        }
+        return _resolverInstance.Parse(stringValue);
+    }
+}
+public interface IParameterParserResolver
+{
+    object? Parse(string? stringValue);
+}
