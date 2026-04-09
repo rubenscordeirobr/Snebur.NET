edf89bf8103dea6006d4cec7f455bb747d8e31da
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-09 11:45:24 +0200

    feat: add RuntimeServicesConfiguration for service registration

---
+++ src/Snebur.RuntimeServices/RuntimeServicesConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Snebur.Application.Abstractions.Security;
+using Snebur.RuntimeServices.Providers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Snebur.RuntimeServices;
+
+public static class RuntimeServicesConfiguration
+{
+    public static IServiceCollection AddRuntimeServices(
+        this IServiceCollection services)
+    {
+        services.AddSingleton<ICommandTrackingService, CommandTrackingService>()
+            .AddSingleton<IUserSessionCacheService, UserSessionCacheService>()
+            .AddSingleton<IUserSessionTokenHandler, UserSessionTokenHandler>()
+            .AddSingleton<IEntityAuthorizationService, EntityAuthorizationService>()
+            .AddSingleton<IAuthenticationAttemptLimiterService, AuthenticationAttemptLimiterService>()
+            .AddSingleton<IJsonStringLocalizerService, JsonStringLocalizerService>()
+            .AddSingleton<ITranslationService, LibreTranslationService>()
+            .AddScoped<IRequestMediator, RequestMediator>()
+            .AddScoped<IEventMediator, EventMediator>()
+            .AddScoped<IUserSessionManager, UserSessionManager>()
+            .AddScoped<ICultureProvider, CultureProvider>()
+            .AddTransient<IUserSessionVerificationService, UserSessionVerificationService>();
+
+        return services;
+    }     
+}
