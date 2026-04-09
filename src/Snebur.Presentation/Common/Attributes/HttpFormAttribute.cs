c1be00bccea8c177f8b8f9ff7955752ea6b5341d
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-09 11:44:22 +0200

    feat: add HttpFormAttribute for handling POST requests in attributes

---
+++ src/Snebur.Presentation/Common/Attributes/HttpFormAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Snebur.Presentation.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+public sealed class HttpFormAttribute : HttpMethodAttribute
+{
+    public override HttpVerb HttpVerb
+        => HttpVerb.Post;
+
+    public HttpFormAttribute(string operationTemplate = "")
+        : base(HttpStatusCode.OK, "", operationTemplate) { }
+}
