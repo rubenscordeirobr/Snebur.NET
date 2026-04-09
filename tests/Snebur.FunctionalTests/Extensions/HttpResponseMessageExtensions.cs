b75323e954e3fab21382ce5c51903803d54fadd9
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-09 11:50:54 +0200

    feat(tests): add HttpResponseMessageExtensions and test runsettings file

---
+++ tests/Snebur.FunctionalTests/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Snebur.FunctionalTests.Extensions;
+
+public static class HttpResponseMessageExtensions
+{
+    public static void MediaTypeShouldBeApplicationJson(this HttpResponseMessage response)
+    {
+        var mediaType = response.Content?.Headers?.ContentType?.MediaType;
+        mediaType.Should().Be("application/json");
+
+    }
+}
+
