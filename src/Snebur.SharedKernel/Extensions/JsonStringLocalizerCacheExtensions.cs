f580be9c29a99ca2798caee178e1ec5c229414da
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-14 08:57:44 +0200

    feat: add JsonStringLocalizerCache extensions for language loading functionality

---
+++ src/Snebur.SharedKernel/Extensions/JsonStringLocalizerCacheExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Snebur.SharedKernel.Abstractions;
+
+namespace Snebur.SharedKernel.Extensions;
+
+public static class JsonStringLocalizerCacheExtensions
+{
+    public static Task EnsureSystemLanguageLoadedAsync(
+        this IJsonStringLocalizerCache localizerCache)
+    {
+        Guard.NotNull(localizerCache);
+        return localizerCache.EnsureLanguageLoadedAsync(LanguageHelper.SystemLanguage);
+    }
+    public static Task EnsureLanguageLoadedAsync(
+        this IJsonStringLocalizerCache localizerCache,
+        Culture culture)
+    {
+        Guard.NotNull(localizerCache);
+
+        var language = CultureHelper.GetLanguage(culture);
+        return localizerCache.EnsureLanguageLoadedAsync(language);
+    }
+}
