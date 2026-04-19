86824fc45b45b2b839764942c8fb2ac883d78fa3
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 19:01:07 +0200

    feat: add InternationalDialingCodeExtensions for retrieving dialing codes

---
+++ src/Snebur.Core/Exceptions/InternationalDialingCodeExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Snebur.Core.Exceptions;
+public static class InternationalDialingCodeExtensions
+{
+    public static string GetDialingCode(this InternationalDialingCode code)
+    {
+        var dialingCodeAttribute = code.GetCustomAttribute<DialingCodeAttribute>();
+        if (dialingCodeAttribute is null)
+        {
+            throw new MissingAttributeException(typeof(DialingCodeAttribute).Name, code.GetType().Name);
+        }
+        return dialingCodeAttribute.Code;
+    }
+}
