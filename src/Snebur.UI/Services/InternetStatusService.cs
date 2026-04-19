7293d43ba77a8f286dbc406e15634174b3d01d98
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 13:51:02 +0200

    feat: add InternetStatusService to check and monitor internet connection status

---
+++ src/Snebur.UI/Services/InternetStatusService.cs
@@ -0,0 +1,54 @@
+﻿using Snebur.ClientGateway.Common.Abstractions;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+
+namespace Snebur.UI.Services;
+
+public class InternetStatusService : IInternetStatusService
+{
+    private readonly IJSRuntime _jsRuntime;
+    private readonly ILogger<InternetStatusService> _logger;
+
+    public InternetStatusService(
+        ILogger<InternetStatusService> logger,
+        IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+        _logger = logger;
+    }
+
+    public async Task<bool> CheckInternetConnectionAsync()
+    {
+        try
+        {
+            if (!_jsRuntime.IsJsRuntimeInitialized())
+            {
+                return await CheckInternetConnectionServerSideAsync();
+            }
+
+            return await _jsRuntime.InvokeAsync<bool>("eval", "navigator.onLine");
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError(ex, "Error checking internet connection");
+            return false;
+        }
+    }
+
+    private Task<bool> CheckInternetConnectionServerSideAsync()
+    {
+        // Implement server-side logic to check internet connection
+        // This is a placeholder implementation
+
+        // In a real-world scenario, you might want to ping a known server or check a specific endpoint
+        return Task.FromResult(true);
+    }
+
+    public async Task WaitForInternetConnectionAsync()
+    {
+        while (!await CheckInternetConnectionAsync())
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
