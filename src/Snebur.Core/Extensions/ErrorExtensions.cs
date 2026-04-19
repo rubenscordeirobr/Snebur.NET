71c6f4ac74f9a050ad8bd58ccd54aef3957f4a4b
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 12:59:32 +0200

    feat: add ErrorExtensions class for generating error codes

---
+++ src/Snebur.Core/Extensions/ErrorExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Snebur.Core.Extensions;
+
+public static class ErrorExtensions
+{
+    public static string GetErrorCode<T>(this object _, string memberName)
+    {
+        return ErrorCodeFactory.CreateCodeFor<T>(memberName);
+    }
+
+    public static string GetErrorCode(this object obj, string memberName)
+    {
+        Guard.NotNull(obj);
+        return ErrorCodeFactory.CreateCodeFor(obj.GetType(), memberName);
+    }
+}
+
