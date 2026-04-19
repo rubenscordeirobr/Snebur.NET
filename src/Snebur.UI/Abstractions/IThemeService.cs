e25bd1a207b9c370826da35c3f1e3803f6bc8410
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 13:47:32 +0200

    feat: add IThemeService and implementation for theme management

---
+++ src/Snebur.UI/Abstractions/IThemeService.cs
@@ -0,0 +1,22 @@
+﻿namespace Snebur.UI.Abstractions;
+
+public interface IThemeService
+{
+    // Events
+    event Action OnThemeModeChanged;
+    event Action OnOfficeColorChanged;
+
+    // Properties
+    DesignThemeModes ThemeMode { get; }
+    OfficeColor OfficeColor { get; }
+
+    string ThemeColor { get; }
+    bool IsDarkMode { get; set; }
+
+    // Methods
+    Task InicializeAsync();
+
+    void SetThemeMode(DesignThemeModes themeMode);
+    void SetOfficeColor(OfficeColor officeColor);
+  
+}
---
+++ src/Snebur.UI/Services/ThemeService.cs
@@ -0,0 +1,42 @@
+﻿using Snebur.UI.Core;
+
+namespace Snebur.UI.Services;
+
+public class ThemeService : IThemeService
+{
+    public DesignThemeModes ThemeMode { get; private set; } = DefaultLayoutConstants.ThemeMode;
+
+    public string ThemeColor { get; private set; } = DefaultLayoutConstants.ThemeColor;
+    public OfficeColor OfficeColor { get; private set; } = DefaultLayoutConstants.DefaultOfficeColor;
+
+    public event Action OnThemeModeChanged;
+    public event Action OnOfficeColorChanged;
+
+    public ThemeService()
+    {
+        //Check colocar after 30 secu
+    }
+
+    public Task InicializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public bool IsDarkMode
+    {
+        get => ThemeMode == DesignThemeModes.Dark;
+        set => SetThemeMode(value ? DesignThemeModes.Dark : DesignThemeModes.Light);
+    }
+
+    public void SetThemeMode(DesignThemeModes themeMode)
+    {
+        ThemeMode = themeMode;
+        OnThemeModeChanged?.Invoke();
+    }
+
+    public void SetOfficeColor(OfficeColor officeColor)
+    {
+        OfficeColor = officeColor;
+        OnOfficeColorChanged?.Invoke();
+    }
+}
