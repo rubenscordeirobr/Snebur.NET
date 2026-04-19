4ab71f419cdd4bcddcea9765e6f28e26ac15d219
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 13:48:54 +0200

    feat: add vertical space component and layout constants for styling

---
+++ src/Snebur.UI/Core/DefaultLayoutConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Snebur.UI.Core;
+
+public static class DefaultLayoutConstants
+{
+    public const DesignThemeModes ThemeMode = DesignThemeModes.Light;
+    public const OfficeColor DefaultOfficeColor = OfficeColor.Excel;
+    public const string ThemeColor = "red";
+}
---
+++ src/Snebur.UI/Core/SpacingSizes.cs
@@ -0,0 +1,15 @@
+﻿namespace Snebur.UI.Core;
+
+public static class GapSizes
+{
+    public const int None = 0;
+    public const int ExtraSmall = 2;
+    public const int Small = 4;
+    public const int Medium = 8;
+    public const int Large = 16;
+    public const int ExtraLarge = 32;
+    public const int DoubleExtraLarge = 64;
+    public const int TripleExtraLarge = 128;
+    public const int Huge = 256;
+}
+
