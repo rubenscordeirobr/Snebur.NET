1bfeed0d54eabfcaaa09fa9c1f301689492571e1
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 13:55:30 +0200

    feat: add ToggleThemeMode extension method for IThemeService

---
+++ src/Snebur.UI/Extensions/ThemeServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Snebur.UI.Extensions;
+
+public static class ThemeServiceExtensions
+{
+    public static void ToggleThemeMode(this IThemeService themeService)
+    {
+        Guard.NotNull(themeService);
+
+        var nextThemeState = themeService.ThemeMode == DesignThemeModes.Light
+            ? DesignThemeModes.Dark
+            : DesignThemeModes.Light;
+
+        themeService.SetThemeMode(nextThemeState);
+    }
+}
+
