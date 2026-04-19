47e11a6d09566cfe2d92639b1ad1c085e0eb2a9b
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 13:50:33 +0200

    feat: add AnonymousPage and AuthenticatedPage base classes for authorization handling

---
+++ src/Snebur.UI/Components/Pages/AnonymousPage.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Snebur.UI.Components.Pages;
+
+[AllowAnonymous]
+public abstract partial class AnonymousPage : PageBase
+{
+}
+
---
+++ src/Snebur.UI/Components/Pages/AuthenticatedPage.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Snebur.UI.Components.Pages;
+
+[Authorize]
+public abstract partial class AuthenticatedPage : PageBase
+{
+}
+
