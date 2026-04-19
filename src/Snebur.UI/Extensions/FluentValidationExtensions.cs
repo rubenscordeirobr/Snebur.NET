c71aeb37e8724f907914768d54789676c3688c01
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 13:54:58 +0200

    feat: add FluentValidation extensions for custom validation logic

---
+++ src/Snebur.UI/Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Snebur.UI.Extensions;
+
+public static class FluentValidationExtensions
+{
+    public static void ValidateWithCommand<T, TCommand>(
+        this IRuleBuilder<T, T> ruleBuilder,
+        Func<T, TCommand> commandFactory,
+        IValidator<TCommand> validator)
+    {
+       ruleBuilder.CustomAsync(async (model, context, cancellation) =>
+      {
+          var result = await validator.ValidateAsync(commandFactory(model), cancellation);
+          if (!result.IsValid)
+          {
+              foreach (var error in result.Errors)
+              {
+                  context.AddFailure(error.PropertyName, error.ErrorMessage);
+              }
+          }
+      });
+    }
+}
