f1b12880da3e933bbc4cee22435d35140421c264
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 13:55:15 +0200

    feat: add NavigationManager extensions for safe navigation handling

---
+++ src/Snebur.UI/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Snebur.UI.Extensions;
+
+public static class NavigationManagerExtensions
+{
+    public static void TryNavigateTo(this NavigationManager navigation,
+        [StringSyntax(StringSyntaxAttribute.Uri)] string uri, 
+        bool forceLoad = false, 
+        bool replace = false)
+    {
+        if (navigation is null)
+        {
+            return;
+        }
+
+        try
+        {
+            navigation.NavigateTo(uri, forceLoad, replace);
+        }
+        catch
+        {
+            //ignore
+        }
+    }
+
+     public static void TryNavigateTo(
+         this NavigationManager navigation, 
+         [StringSyntax(StringSyntaxAttribute.Uri)] string uri,
+         NavigationOptions options)
+    {
+        if (navigation is null)
+        {
+            return;
+        }
+
+        try
+        {
+            navigation.NavigateTo(uri, options);
+        }
+        catch 
+        {
+            //ignore
+        }
+    }
+}
+
