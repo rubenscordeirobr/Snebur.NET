4d51bc08df8e2cfeb507be71417ac5b96e29f98a
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-19 13:51:44 +0200

    feat: add RequestErrorNotifier service for handling and displaying request errors

---
+++ src/Snebur.UI/Services/RequestErrorNotifier.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using Snebur.ClientGateway.Common.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace Snebur.UI.Services;
+
+public class RequestErrorNotifier : IRequestErrorNotifier
+{
+    private readonly ILogger<RequestErrorNotifier> _logger;
+    private readonly IDialogService _dialogService;
+    public RequestErrorNotifier(
+        IDialogService dialogService,
+        ILogger<RequestErrorNotifier> logger)
+    {
+        _dialogService = dialogService;
+        _logger = logger;
+    }
+
+    public async Task NotifyRequestErrorAsync(Error error, Uri requestUri)
+    {
+        if (IsShowErrorDialog(error?.StatusCode))
+        {
+            var errorMessage = error?.Message ?? "Unknown error";
+            try
+            {
+                var dialog = await _dialogService.ShowErrorAsync(errorMessage, "Oops", "OK");
+                await dialog.Result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error showing error dialog. {ErrorMessage}. Error: {Error}",
+                    errorMessage,
+                    ex.GetNestedMessage());
+            }
+        }
+    }
+
+    public static bool IsShowErrorDialog(HttpStatusCode? httpStatusCode)
+    {
+        if (httpStatusCode is null)
+            return true;
+
+        return httpStatusCode switch
+        {
+            HttpStatusCode.NotFound => false,
+            HttpStatusCode.Unauthorized => false,
+            _ => true
+        };
+
+    }
+}
