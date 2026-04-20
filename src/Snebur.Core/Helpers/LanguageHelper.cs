585b0070290651c4d67f1c9e2925891d1d704d50
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-20 07:39:19 +0200

    feat: add LanguageHelper and ParameterLanguageParserResolver classes for language management

---
+++ src/Snebur.Core/Helpers/LanguageHelper.cs
@@ -0,0 +1,49 @@
+﻿
+using Snebur.Core.Mappers;
+
+namespace Snebur.Core.Helpers;
+
+public static class LanguageHelper
+{
+    public static Language SystemLanguage => Language.English;
+
+    public static Language? Normalize(Culture culture, Language? language)
+    {
+        if(language is null)
+            return null;
+
+        return Normalize(culture, language.Value);
+    }
+
+    public static Language Normalize(Culture culture, Language language)
+    {
+        return language == Language.Default
+          ? CultureMapper.MapLanguage(culture)
+          : language;
+    }
+
+    public static Language GetLanguage(string? languageCode)
+    {
+        var language = LanguageMapper.MapLanguage(languageCode);
+        if (language.HasValue)
+        {
+            return language.Value;
+        }
+
+        var culture = CultureMapper.MapCulture(languageCode);
+        if (culture.HasValue)
+        {
+            return CultureHelper.GetLanguage(culture.Value);
+        }
+        return SystemLanguage;
+    }
+
+    public static bool IsLanguageCodeSupported(string? languageCode)
+    {
+        if (languageCode is null || languageCode.Length < 2)
+            return false;
+
+        var language = LanguageMapper.MapLanguage(languageCode);
+        return language != null;
+    }
+}
---
+++ src/Snebur.Presentation/Resolver/ParameterLanguageParserResolver.cs
@@ -0,0 +1,12 @@
+﻿using Snebur.Core.Attributes;
+using Snebur.Core.Helpers;
+
+namespace Snebur.Presentation.Resolver;
+
+public class ParameterLanguageParserResolver : IParameterParserResolver
+{
+    public object? Parse(string? stringValue)
+    {
+        return LanguageHelper.GetLanguage(stringValue);
+    }
+}
