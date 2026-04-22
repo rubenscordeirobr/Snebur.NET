ea4613ceff1bfd3cc329f9dc365c64a98f4a4378
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-04-22 12:19:21 +0200

    feat: add IClientUserSessionManager interface for handling user session tokens

---
+++ src/Snebur.ClientGateway/Abstractions/IClientUserSessionManager.cs
@@ -0,0 +1,17 @@
+﻿using Snebur.SharedKernel.Models.Security;
+
+namespace Snebur.ClientGateway.Abstractions;
+
+public interface IClientAuthorizationTokenManager
+{
+    UserSessionClaims? GetUserSessionClaims();
+    UserSessionState? GetUserSessionState();
+
+    Task SetAuthorizationTokenAsync(string authorizationToken, bool isPersistent);
+    Task ValidateAuthorizationHeaderAsync(string responseAuthorizationHeader);
+    Task<string?> GetAuthorizationTokenAsync();
+    Task EnsureUserSessionStateAsync();
+    Task RemoveAuthorizationTokenAsync();
+
+    event EventHandlerAsync<AuthorizationTokenUpdatedEventArgs>? UserSessionStateUpdated;
+}
