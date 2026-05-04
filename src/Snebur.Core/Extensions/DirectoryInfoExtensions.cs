5df2c0146480f35a648828b831e285a819ef035e
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-04 13:17:12 +0200

    feat: add DirectoryInfoExtensions with methods to get required parent directory

---
+++ src/Snebur.Core/Extensions/DirectoryInfoExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Snebur.Core.Extensions;
+
+public static class DirectoryInfoExtensions
+{
+    public static DirectoryInfo GetRequiredParent(
+        this DirectoryInfo directory,
+        string directoryName)
+    {
+        Guard.NotNull(directory);
+        Guard.NotNullOrWhiteSpace(directoryName);
+
+        var parent = directory.GetParent(directoryName);
+        if (parent == null)
+        {
+            throw new DirectoryNotFoundException($"Could not locate the '{directoryName}' directory from the {directory.FullName} directory.");
+        }
+        return parent;
+    }
+
+    public static DirectoryInfo? GetParent(
+        this DirectoryInfo directory,
+        string directoryName)
+    {
+        var current = directory;
+        while (current != null && !current.Name.Equals(directoryName, StringComparison.OrdinalIgnoreCase))
+        {
+            current = current.Parent;
+        }
+        return current;
+    }
+}
