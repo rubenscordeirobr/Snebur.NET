504fd76d37be6621573dcef8c51f0287821bfcd6
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-04 13:20:33 +0200

    feat: implement IRouteService and RouteService for localized URI handling

---
+++ src/Snebur.UI/Abstractions/IRouteService.cs
@@ -0,0 +1,6 @@
+﻿namespace Snebur.UI.Abstractions;
+
+public interface IRouteService
+{
+    string GetLocalizedUri(string? uri);
+}
---
+++ src/Snebur.UI/Services/RouteService.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Web;
+using Snebur.SharedKernel.Extensions;
+
+namespace Snebur.UI.Services;
+
+public class RouteService : IRouteService
+{
+    private readonly ICultureProvider _cultureProvider;
+
+    public RouteService(ICultureProvider cultureProvider)
+    {
+        _cultureProvider = cultureProvider;
+    }
+
+    public string GetLocalizedUri(string? uri)
+    {
+        var localizedUri = GetLocalizedUriInternal(uri);
+        if (_cultureProvider.IsLanguageMatchingCulture())
+        {
+            return localizedUri;
+        }
+
+        Guard.NotNull(localizedUri);
+
+        var languageCode = _cultureProvider.GetLanguageCode();
+        var parts = localizedUri.Split('?');
+        var baseUri = parts[0];
+        var queryString = parts.Length > 1 ? parts[1] : string.Empty;
+
+        var queryParams = HttpUtility.ParseQueryString(queryString);
+        queryParams["lang"] = languageCode;
+        return $"{baseUri}?{queryParams}";
+    }
+
+    private string GetLocalizedUriInternal(string? uri)
+    {
+        var cultureCode = _cultureProvider.GetCultureCode();
+        if (uri is null)
+        {
+            return $"/{cultureCode}";
+        }
+
+        var currentCulture = LocalizedUriUtils.ExtractCultureCodeFromUri(uri);
+        if (cultureCode.Equals(currentCulture, StringComparison.OrdinalIgnoreCase))
+        {
+            return uri;
+        }
+
+        return LocalizedUriUtils.BuildLocalizedUri(uri, cultureCode!);
+    }
+}
