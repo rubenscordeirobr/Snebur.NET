c07b325703b48413a3e15b99282705cf0a999293
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-11 13:28:39 -0300

    feat: add interfaces for client admin and tenant user session context services

---
+++ src/Snebur.ClientGateway/Abstractions/IClientAdminUserSessionContextService.cs
@@ -0,0 +1,16 @@
+﻿using Snebur.SharedKernel.Models.Security;
+
+namespace Snebur.ClientGateway.Abstractions;
+
+public interface IClientAdminUserSessionContextService : IClientUserSessionContextService
+{
+    AdminUserSessionContext? SessionContext { get; }
+    Task<AdminUserSessionContext?> GetSessionContextAsync(); 
+
+    Task SetSessionContextAsync(AdminUserSessionContext sessionContext, bool isPersistent);
+}
+
+public sealed record AdminUserSessionContext(
+        UserSessionClaims UserSessionClaims,
+        UserSessionResponse UserSession,
+        UserResponse user);
---
+++ src/Snebur.ClientGateway/Abstractions/IClientTenantUserSessionContextService.cs
@@ -0,0 +1,17 @@
+﻿using Snebur.SharedKernel.Models.Security;
+
+namespace Snebur.ClientGateway.Abstractions;
+
+public interface IClientTenantUserSessionContextService: IClientUserSessionContextService
+{
+    TenantUserSessionContext? SessionContext { get; }
+    Task<TenantUserSessionContext?> GetSessionContextAsync();
+
+    Task SetSessionContextAsync(TenantUserSessionContext sessionContext, bool isPersistent);
+}
+
+public sealed record TenantUserSessionContext(
+        UserSessionClaims UserSessionClaims,
+        UserSessionResponse UserSession,
+        UserResponse User,
+        TenantResponse Tenant);
