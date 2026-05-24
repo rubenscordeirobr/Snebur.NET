11517e20da5dec0dea277c99a56a474c0f4d53e6
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-24 18:51:27 -0300

    feat: add IStorageService interface for storage operations

---
+++ src/Snebur.UI/Abstractions/IStorageService.cs
@@ -0,0 +1,10 @@
+﻿namespace Snebur.UI.Abstractions;
+
+public interface IStorageService
+{
+    Task<T?> GetItemAsync<T>(string storageKey);
+    Task<string?> GetItemAsync(string storageKey);
+    Task SetItemAsync(string storageKey, string textValue, bool isPersistent);
+    Task SetItemAsync<T>(string storageKey, T value, bool isPersistent);
+    Task RemoveItemAsync(string storageKey);
+}
