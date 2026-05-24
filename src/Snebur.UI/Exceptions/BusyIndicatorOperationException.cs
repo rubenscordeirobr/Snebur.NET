ea96286910095583a4d5c3c338ce5da0473642fa
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-24 16:58:48 -0300

    feat: add BusyIndicatorOperationException for handling busy indicator errors

---
+++ src/Snebur.UI/Exceptions/BusyIndicatorOperationException.cs
@@ -0,0 +1,14 @@
+﻿namespace Snebur.UI.Exceptions;
+
+public class BusyIndicatorOperationException : Exception
+{
+    public BusyIndicatorOperationException(string message)
+        : base(message)
+    {
+    }
+    public BusyIndicatorOperationException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
+
