8630c78304dbca62987257d2eda938ff29d64b65
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-24 16:02:13 -0300

    feat: add password validation utility class

---
+++ src/Snebur.SharedKernel/Utils/PasswordUtils.cs
@@ -0,0 +1,17 @@
+﻿using Snebur.SharedKernel.Constants;
+
+namespace Snebur.SharedKernel.Utils;
+
+public static class PasswordValidationUtils
+{
+    public static bool IsCreatePasswordValid(string password)
+    {
+        return !string.IsNullOrWhiteSpace(password) &&
+               password.Length >= ValidationConstants.PasswordMinLength &&
+               password.Length <= ValidationConstants.PasswordMaxLength &&
+               password.Any(char.IsUpper) &&
+               password.Any(char.IsLower) &&
+               password.Any(char.IsDigit) &&
+               password.Any(c => !char.IsLetterOrDigit(c));
+    }
+}
