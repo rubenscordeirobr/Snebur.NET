d2ce8b2d1ddc8f77e63a70cbd06161b040a44861
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-24 16:41:45 -0300

    feat: add global suppressions for code analysis across projects

---
+++ src/Snebur.UseCases.Shared/Common/Validations/RuleBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Snebur.Core.Utils;
+
+namespace Snebur.UseCases.Shared;
+
+public static class RuleBuilderExtensions
+{
+    public static string? GetPropertyName<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder)
+    {
+        var rule = ReflectionUtils.GetPropertyValue(ruleBuilder, "Rule");
+        if (rule is not null)
+        {
+            return ReflectionUtils.GetPropertyValue(rule, "PropertyName")?.ToString();
+        }
+        return null;
+    }
+}
+
---
+++ tests/Snebur.Core.UnitTests/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell",
+    "S4144:Methods should not have identical implementations",
+    Justification = "This is a test project and the methods are intentionally identical for testing purposes.",
+    Scope = "module")]
