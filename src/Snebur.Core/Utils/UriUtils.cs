d7116b45098a0cfef39d3e3e9ef563826a3e7271
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-24 13:35:13 -0300

    feat: add UriUtils with IsAbsoluteUri method for URI validation

---
+++ src/Snebur.Core/Utils/UriUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Snebur.Core.Utils;
+
+public static class UriUtils
+{
+    public static bool IsAbsoluteUri(
+        [NotNullWhen(true)]
+        string? uri)
+    {
+        if (uri is null)
+            return false;
+
+        uri= uri.Trim();
+        return uri.StartsWith(Uri.UriSchemeHttp, StringComparison.OrdinalIgnoreCase) ||
+               uri.StartsWith(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase);
+    }
+}
