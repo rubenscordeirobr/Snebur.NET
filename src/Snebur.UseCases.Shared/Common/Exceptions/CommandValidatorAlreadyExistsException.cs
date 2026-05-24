aa385456f026313c9eb24024daef2290fa76d164
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-24 15:58:52 -0300

    feat: add custom exceptions for command validator conflicts and not found scenarios

---
+++ src/Snebur.UseCases.Shared/Common/Exceptions/CommandValidatorAlreadyExistsException.cs
@@ -0,0 +1,8 @@
+﻿namespace Snebur.UseCases.Common.Excpetions;
+
+public class CommandValidatorAlreadyExistsException : Exception
+{
+    public CommandValidatorAlreadyExistsException(string message) : base(message)
+    {
+    }
+}
---
+++ src/Snebur.UseCases.Shared/Common/Exceptions/CommandValidatorNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace Snebur.UseCases.Common.Excpetions;
+
+public class CommandValidatorNotFoundException : Exception
+{
+    public IReadOnlyList<Type> CommandTypes { get; }
+ 
+    public CommandValidatorNotFoundException(string message,
+        IReadOnlyList<Type> commandTypes)
+        : base(message)
+    {
+        CommandTypes = commandTypes;
+    }
+}
