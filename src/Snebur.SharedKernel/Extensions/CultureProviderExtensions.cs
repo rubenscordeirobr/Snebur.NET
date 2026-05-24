b0cb311e1d514ce68d2b2bdc3512c38978ad8031
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-24 13:45:09 -0300

    feat: add extension methods for ICultureProvider functionality

---
+++ src/Snebur.SharedKernel/Extensions/CultureProviderExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Snebur.Core.Mappers;
+using Snebur.SharedKernel.Abstractions;
+
+namespace Snebur.SharedKernel.Extensions;
+
+public static class CultureProviderExtensions
+{
+    public static string GetCultureCode(this ICultureProvider cultureProvider)
+    {
+        Guard.NotNull(cultureProvider);
+
+        return CultureHelper.GetCultureCode(cultureProvider.Culture);
+    }
+
+    public static string GetLanguageCode(this ICultureProvider cultureProvider)
+    {
+        Guard.NotNull(cultureProvider);
+        return LanguageMapper.MapCode(cultureProvider.Language);
+    }
+
+    public static Country GetCountry(this ICultureProvider cultureProvider)
+    {
+        Guard.NotNull(cultureProvider);
+
+        return CultureHelper.GetCountry(cultureProvider.Culture);
+    }
+
+    public static Currency GetCurrency(this ICultureProvider cultureProvider)
+    {
+        Guard.NotNull(cultureProvider);
+
+        return CultureHelper.GetCurrency(cultureProvider.Culture);
+    }
+
+    public static bool IsLanguageMatchingCulture(
+        this ICultureProvider cultureProvider)
+    {
+        Guard.NotNull(cultureProvider);
+
+        var culture = cultureProvider.Culture;
+        var sessionLanguage = cultureProvider.Language;
+        var cultureLanguage = CultureHelper.GetLanguage(culture);
+        return sessionLanguage == cultureLanguage;
+    }
+}
