ae436476293d937d51ea3436acb15ffa942003c3
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-05-24 16:01:39 -0300

    feat: add IUserSessionService interface for user session management

---
+++ src/Snebur.UseCases.Shared/Abstractions/Identities/IUserSessionService.cs
@@ -0,0 +1,10 @@
+﻿using Snebur.UseCases.Identities.UserSessions;
+
+namespace Snebur.UseCases.Abstractions.Identities;
+
+public interface IUserSessionService
+{
+    Task ChangeLanguageAsync(
+        ChangeUserSessionLanguageCommand command, 
+        CancellationToken cancellationToken = default);
+}
